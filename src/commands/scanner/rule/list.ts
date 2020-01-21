b091064a465e30534789496ba1157119dc7b6a8e
Josh Feingold <jfeingold@salesforce.com>
2020-01-21 12:21:18 -0600

    @W-7109964@: scanner:rule:list now outputs an extremely simple, hardcoded table. Next step is to add comments to the existing code so the inner workings are obvious. Then we see where to go next.

--- src/commands/scanner/rule/list.ts
+++ src/commands/scanner/rule/list.ts
@@ -9,6 +9,18 @@ Messages.importMessagesDirectory(__dirname);
 // or any library that is using the messages framework can also be loaded this way.
 const messages = Messages.loadMessages('scanner', 'rule');
 
+enum AuthorFilter {
+  All = 1,
+  StandardOnly,
+  CustomOnly
+}
+
+enum ActivationFilter {
+  All = 1,
+  ActiveOnly,
+  InactiveOnly
+}
+
 export default class List extends SfdxCommand {
 
   public static description = messages.getMessage('list.commandDescription');
@@ -57,7 +69,54 @@ export default class List extends SfdxCommand {
     }),
   };
 
+  private async getRules(type : string, sev : string, langs : string[], author : AuthorFilter, activation : ActivationFilter) : Promise<Object[]> {
+    let rules = [
+      {
+        name: 'Rule 1',
+        type: 'Security',
+        languages: ['JS', 'Apex', 'Java'],
+        author: 'Salesforce',
+        active: 'Active'
+      },
+      {
+        name: 'Rule 2',
+        type: 'Best Practice',
+        languages: ['JS'],
+        author: 'Doofenshmirtz Evil Inc',
+        active: 'Inactive'
+      }
+    ];
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        res(rules);
+      }, 2500);
+    });
+  }
+
+  private validateFlags() : void {
+    if (this.flags.standard && this.flags.custom) {
+      throw Error(messages.getMessage('list.flagValidations.authorFlagsMutex'));
+    }
+    if (this.flags.active && this.flags.inactive) {
+      throw Error(messages.getMessage('list.flagValidations.activationFlagsMutex'));
+    }
+  }
+
   public async run(): Promise<AnyJson> {
-    return {};
+    this.validateFlags();
+
+    const type = this.flags.type;
+    const sev = this.flags.severity;
+    const langs = this.flags.languages;
+    const author = this.flags.standard ? AuthorFilter.StandardOnly : this.flags.custom ? AuthorFilter.CustomOnly : AuthorFilter.All;
+    const activation = this.flags.active ? ActivationFilter.ActiveOnly : this.flags.inactive ? ActivationFilter.InactiveOnly : ActivationFilter.All;
+
+    return this.getRules(type, sev, langs, author, activation)
+      .then((res : Object[]) => {
+        this.ux.table(res, ['name', 'type', 'languages', 'author', 'active']);
+        return {};
+      }, (rej) => {
+        return {};
+      });
   }
 }
