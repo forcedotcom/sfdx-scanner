bf0f6d0683e23a1dbb39bd413533aea345fed0ce
Josh Feingold <jfeingold@salesforce.com>
2020-01-28 16:00:12 -0600

    @W-7159171@: My code can sweep a JAR for all of the category and ruleset files, and it can read those files line-by-line. It does nothing interesting or powerful with any of that yet.

--- src/lib/pmd/pmd-rule-cataloger/src/main/java/sfdc/isv/swat/Main.java
+++ src/lib/pmd/pmd-rule-cataloger/src/main/java/sfdc/isv/swat/Main.java
@@ -2,6 +2,7 @@ package sfdc.isv.swat;
 
 public class Main {
   public static void main(String[] args) {
-
+    PmdRuleCataloger prc = new PmdRuleCataloger();
+    prc.doTheThing(args);
   }
 }
--- src/lib/pmd/pmd-rule-cataloger/src/main/java/sfdc/isv/swat/PmdRuleCataloger.java
+++ src/lib/pmd/pmd-rule-cataloger/src/main/java/sfdc/isv/swat/PmdRuleCataloger.java
@@ -1,7 +1,56 @@
 package sfdc.isv.swat;
 
-import java.io.File;
+import java.io.*;
+import java.net.URL;
 import java.util.*;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
 
 public class PmdRuleCataloger {
+  public void doTheThing(String[] args) {
+    List<String> files = getFilesFromJar(args[0]);
+    logFileContents(files.get(0));
+  }
+
+  private List<String> getFilesFromJar(String jarName) {
+    List<String> foundFiles = new ArrayList<>();
+    try {
+      JarInputStream jstream = new JarInputStream(new FileInputStream(jarName));
+      JarEntry entry;
+
+      while ((entry = jstream.getNextJarEntry()) != null) {
+        String name = entry.getName();
+        if (name.endsWith(".xml") && (name.startsWith("category") || name.startsWith("rulesets"))) {
+          foundFiles.add(name);
+          System.out.println("Found file " + name + " in jar " + jarName);
+        }
+      }
+    } catch (FileNotFoundException fnf) {
+      System.out.println("FileNotFound Exception: " + fnf.getMessage());
+    } catch (IOException io) {
+      System.out.println("IOException: " + io.getMessage());
+    }
+    return foundFiles;
+  }
+
+  private void logFileContents(String path) {
+    List<String> contents = new ArrayList<>();
+    try (
+      InputStream in = getResourceAsStream(path);
+      BufferedReader br = new BufferedReader(new InputStreamReader(in))
+    ) {
+      String resource;
+      while ((resource = br.readLine()) != null) {
+        System.out.println(resource);
+        contents.add(resource);
+      }
+    } catch (IOException io) {
+      System.out.println("IOException: " + io.getMessage());
+    }
+  }
+
+  private InputStream getResourceAsStream(String path) {
+    final InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
+    return in == null ? getClass().getResourceAsStream(path) : in;
+  }
 }
