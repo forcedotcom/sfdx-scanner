8ca7263f2dc4dafe1da292735d3d089d4c3cbbc1
Josh Feingold <jfeingold@salesforce.com>
2020-01-28 14:36:18 -0600

    @W-7159171@: Added Java module for cataloger. Need to determine best way to scan in the rules. So many different options, jeez.

---
+++ src/lib/pmd/pmd-rule-cataloger/src/main/java/sfdc/isv/swat/Main.java
@@ -0,0 +1,7 @@
+package sfdc.isv.swat;
+
+public class Main {
+  public static void main(String[] args) {
+
+  }
+}
---
+++ src/lib/pmd/pmd-rule-cataloger/src/main/java/sfdc/isv/swat/PmdRuleCataloger.java
@@ -0,0 +1,7 @@
+package sfdc.isv.swat;
+
+import java.io.File;
+import java.util.*;
+
+public class PmdRuleCataloger {
+}
