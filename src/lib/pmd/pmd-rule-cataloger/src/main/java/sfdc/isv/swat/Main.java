9e71b32497d73bef4a1b32141cf99176df6542a2
Joshua Feingold <jfeingold@salesforce.com>
2020-01-29 13:19:10 -0600

    @W-7159171@: RuleCataloger can now identify all necessary files and parse them into Document objects, but it cannot yet properly analyze the nodes.

--- src/lib/pmd/pmd-rule-cataloger/src/main/java/sfdc/isv/swat/Main.java
+++ src/lib/pmd/pmd-rule-cataloger/src/main/java/sfdc/isv/swat/Main.java
@@ -1,8 +1,16 @@
 package sfdc.isv.swat;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
   public static void main(String[] args) {
-    PmdRuleCataloger prc = new PmdRuleCataloger();
-    prc.doTheThing(args);
+    // TODO: We want the set of languages to be configurable in some way. So this should be dynamically populated either
+    //  through arguments or by reading a file.
+    List<String> supportedLangs = new ArrayList<>(Arrays.asList("javascript", "apex"));
+
+    PmdRuleCataloger prc = new PmdRuleCataloger("6.20.0", "./dist/pmd/lib", supportedLangs);
+    prc.catalogRules();
   }
 }
--- src/lib/pmd/pmd-rule-cataloger/src/main/java/sfdc/isv/swat/PmdRuleCataloger.java
+++ src/lib/pmd/pmd-rule-cataloger/src/main/java/sfdc/isv/swat/PmdRuleCataloger.java
@@ -1,56 +1,179 @@
 package sfdc.isv.swat;
 
 import java.io.*;
-import java.net.URL;
 import java.util.*;
 import java.util.jar.JarEntry;
 import java.util.jar.JarInputStream;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
-public class PmdRuleCataloger {
-  public void doTheThing(String[] args) {
-    List<String> files = getFilesFromJar(args[0]);
-    logFileContents(files.get(0));
+import javax.xml.parsers.*;
+
+
+class PmdRuleCataloger {
+  private String pmdVersion;
+  private String pmdPath;
+  private List<String> languages;
+  private Map<String,List<String>> categoryFilesByLanguage = new HashMap<>();
+  private Map<String,List<String>> rulesetFilesByLanguage = new HashMap<>();
+
+
+  /**
+   *
+   * @param pmdVersion   - The version of PMD being used, e.g. 6.20.0.
+   * @param pmdPath      - The path to PMD's lib folder.
+   * @param languages    - The languages whose rules should be catalogued.
+   */
+  PmdRuleCataloger(String pmdVersion, String pmdPath, List<String> languages) {
+    this.pmdVersion = pmdVersion;
+    this.pmdPath = pmdPath;
+    this.languages = languages;
   }
 
-  private List<String> getFilesFromJar(String jarName) {
-    List<String> foundFiles = new ArrayList<>();
+
+  /**
+   * TODO: WRITE A REAL HEADER FOR THIS FUNCTION.
+   */
+  void catalogRules() {
+    // STEP 1: Identify all of the ruleset and category files for each language we're looking at.
+    for (String language : this.languages) {
+      this.mapFileNamesByLanguage(language);
+    }
+
+    // STEP 2: Sweep the category files and pull out their rule definitions.
+    for (List<String> cats : categoryFilesByLanguage.values()) {
+      for (String cat : cats) {
+        deriveRulesFromCategoryFile(cat);
+      }
+      break;
+    }
+
+    /*
+    remaining steps:
+    - parse the category files to get information about the rules.
+    - parse the ruleset files to figure out which rules belong to which sets.
+    - write it all out to a file somewhere.
+     */
+
+    // TODO: REPLACE THIS LOG WITH THE REST.
+    System.out.println("Category map");
+    System.out.println(this.categoryFilesByLanguage.toString());
+    System.out.println("Ruleset map");
+    System.out.println(this.rulesetFilesByLanguage.toString());
+  }
+
+
+  /**
+   * Returns the name of the JAR in which PMD stores rule definitions for the specified language.
+   * @param language - The language for which we should find the corresponding PMD JAR.
+   * @return         - The name of the corresponding JAR.
+   */
+  private String deriveJarNameForLanguage(String language) {
+    return "pmd-" + language + "-" + this.pmdVersion + ".jar";
+  }
+
+
+  /**
+   * Identifies the JAR associated with the given language, and puts all category and ruleset definition files into the
+   * corresponding Map field.
+   * @param language - The language that should be processed.
+   */
+  private void mapFileNamesByLanguage(String language) {
+    // First, define our lists.
+    List<String> categoryFiles = new ArrayList<>();
+    List<String> rulesetFiles = new ArrayList<>();
+
+    // Next, we need to determine the path to the JAR we want to inspect, and then do that inspection.
+    String jarPath = this.pmdPath + File.separator + this.deriveJarNameForLanguage(language);
     try {
-      JarInputStream jstream = new JarInputStream(new FileInputStream(jarName));
+      // Read in the JAR file.
+      JarInputStream jstream = new JarInputStream(new FileInputStream(jarPath));
       JarEntry entry;
 
+      // Iterate over every entry in the stream. These are the names of files/directories in that JAR.
       while ((entry = jstream.getNextJarEntry()) != null) {
-        String name = entry.getName();
-        if (name.endsWith(".xml") && (name.startsWith("category") || name.startsWith("rulesets"))) {
-          foundFiles.add(name);
-          System.out.println("Found file " + name + " in jar " + jarName);
+        String fName = entry.getName();
+        // We only care about .xml files, since those are how rulesets and categories are defined.
+        if (fName.endsWith(".xml")) {
+          // Rulesets all live in "rulesets/**/*.xml", and Categories live in "category/**/*.xml". It's frustrating that
+          // one's plural and the other isn't, but sometimes life involves compromises.
+          if (fName.startsWith("category")) {
+            categoryFiles.add(fName);
+          } else if (fName.startsWith("rulesets")) {
+            rulesetFiles.add(fName);
+          }
         }
       }
     } catch (FileNotFoundException fnf) {
-      System.out.println("FileNotFound Exception: " + fnf.getMessage());
+      // TODO: Better error handling here.
+      System.out.println("FileNotFound exception: " + fnf.getMessage());
     } catch (IOException io) {
+      // TODO: Better error handling here.
       System.out.println("IOException: " + io.getMessage());
     }
-    return foundFiles;
+
+    // Finally, map the files we found by the language name.
+    this.categoryFilesByLanguage.put(language, categoryFiles);
+    this.rulesetFilesByLanguage.put(language, rulesetFiles);
   }
 
-  private void logFileContents(String path) {
-    List<String> contents = new ArrayList<>();
-    try (
-      InputStream in = getResourceAsStream(path);
-      BufferedReader br = new BufferedReader(new InputStreamReader(in))
-    ) {
-      String resource;
-      while ((resource = br.readLine()) != null) {
-        System.out.println(resource);
-        contents.add(resource);
-      }
-    } catch (IOException io) {
-      System.out.println("IOException: " + io.getMessage());
-    }
+
+  /**
+   * TODO: WRITE A REAL HEADER FOR THIS FUNCTION.
+   * @param path
+   */
+  private void deriveRulesFromCategoryFile(String path) {
+    System.out.println("======");
+    System.out.println("Parsing category file: " + path);
+
+    // The category file is an XML, so we'll need to parse that into a Document for us to analyze.
+    Document doc = getDocumentFromPath(path);
+
+    // Next, we'll want to pull off our root element (technically of type "ruleset"), and get a list of all of the 'rule' nodes.
+    Element catNode = doc.getDocumentElement();
+    System.out.println("The category's name is: " + catNode.getTagName());
+    NodeList ruleNodes = catNode.getElementsByTagName("rule");
+    System.out.println("The category defines this many rules: " + ruleNodes.getLength());
   }
 
+
+  /**
+   * Given the path to a resource, returns an InputStream for that resource.
+   * @param path - The path to a resource.
+   * @return     - An InputStream for the provided resource.
+   */
   private InputStream getResourceAsStream(String path) {
     final InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
     return in == null ? getClass().getResourceAsStream(path) : in;
+  }
+
+
+  /**
+   * Accepts the path to an XML resource, and returns a Document.
+   * @param path - The path to an XMML resource.
+   * @return     - A Document object representing the parsed resource.
+   */
+  private Document getDocumentFromPath(String path) {
+    Document doc = null;
+    try (
+      InputStream in = getResourceAsStream(path)
+    ) {
+      DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+      DocumentBuilder db = dbf.newDocumentBuilder();
+      doc = db.parse(in);
+    } catch (IOException ioe) {
+      // TODO: Better error handling here.
+      System.out.println("IOException: " + ioe.getMessage());
+      System.exit(1);
+    } catch (ParserConfigurationException pce) {
+      // TODO: Better error handling here.
+      System.out.println("ParserConfigurationException: " + pce.getMessage());
+      System.exit(1);
+    } catch (SAXException saxe) {
+      // TODO: Better error handling here.
+      System.out.println("SAXException: " + saxe.getMessage());
+      System.exit(1);
+    }
+    return doc;
   }
 }
