76a0d5e01c519be983cbbd4b7787bc370619a827
Josh Feingold <jfeingold@salesforce.com>
2020-03-03 10:40:37 -0600

    Added comment explaining rationale for file renaming.

--- test/commands/scannerbasetest.ts
+++ test/commands/scannerbasetest.ts
@@ -4,11 +4,13 @@ import fs = require('fs');
 
 
 const test = basetest.do(() => {
+  // Before the tests run, we rename the existing custom rule registry so it doesn't interfere with the imminent tests.
   if (fs.existsSync(CUSTOM_CLASSPATH_REGISTER)) {
     fs.renameSync(CUSTOM_CLASSPATH_REGISTER, CUSTOM_CLASSPATH_REGISTER_TMP);
   }
 })
 .finally(ctx => {
+  // Regardless of whether the tests passed or failed, we need to restore the registry to its original state.
   if (fs.existsSync(CUSTOM_CLASSPATH_REGISTER_TMP)) {
     fs.renameSync(CUSTOM_CLASSPATH_REGISTER_TMP, CUSTOM_CLASSPATH_REGISTER);
   }
