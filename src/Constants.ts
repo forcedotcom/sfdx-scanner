78b1e0689864419f11e783d6783b038337852e94
Roopa Mohan <rmohan@rmohan-ltm4.internal.salesforce.com>
2020-03-13 15:22:34 -0700

    Verify JRE setup basics

--- src/Constants.ts
+++ src/Constants.ts
@@ -4,3 +4,4 @@ import path = require('path');
 export const SFDX_SCANNER_PATH = path.join(os.homedir(), '.sfdx-scanner');
 export const PMD_CATALOG = 'PmdCatalog.json';
 export const CUSTOM_PATHS = 'CustomPaths.json';
+export const CONFIG = 'Config.json';
