fed89a0d39cf608774574afd469fac1acdf4a384
rmohan20 <58573547+rmohan20@users.noreply.github.com>
2020-03-16 11:44:22 -0700

    Rm/verify jre (#39)
    * Verify JRE setup basics
    
    * Tying lose ends to verify setup JRE at postinstall, and get java home before triggering a java command.
    Also, modifying email on commit.
    
    Co-authored-by: Roopa Mohan <rmohan@rmohan-ltm4.internal.salesforce.com> - can't make this invalid email disappear from PR even after amending email on the older commits.

---
+++ src/lib/JreSetupManager.ts
@@ -0,0 +1,183 @@
+import { CONFIG } from '../Constants';
+import { ConfigFile, Logger, SfdxError, Messages } from '@salesforce/core';
+import { AsyncCreatable } from '@salesforce/kit';
+
+import process = require('process');
+import findJavaHome = require('find-java-home');
+import childProcess = require('child_process');
+import path = require('path');
+import { FileHandler } from './FileHandler';
+
+// Initialize Messages with the current plugin directory
+Messages.importMessagesDirectory(__dirname);
+
+const JAVA_HOME_KEY = 'java-home';
+const JAVA_HOME_SYSTEM_VARIABLES = ['JAVA_HOME', 'JRE_HOME', 'JDK_HOME'];
+
+class Config extends ConfigFile<ConfigFile.Options> {
+
+    public static getFileName(): string {
+        // TODO: Revisit the file location!
+        // This doesn't work if I give the directory as ~/.sfdx-scanner.
+        // File only goes to ~/.sfdx
+        return CONFIG;
+    }
+}
+
+class JreSetupManager extends AsyncCreatable {
+    private logger!: Logger;
+    private config!: Config;
+
+    protected async init(): Promise<void> {
+        this.logger = await Logger.child('verifyJRE');
+        this.config = await Config.create({
+            isGlobal: true,
+            throwOnNotFound: false
+        });
+    }
+
+    async verifyJreSetup(): Promise<string> {
+        // Find Java Home
+        const javaHome = await this.findJavaHome();
+
+        // If we have javaHome, verify that it is a valid path
+        await this.verifyPath(javaHome);
+
+        // If path exists, verify Java version
+        await this.verifyJavaVersion(javaHome);
+
+        // Write javaHome to Config file
+        await this.setJavaHomeInConfig(javaHome);
+
+        return javaHome;
+    }
+
+    private async findJavaHome(): Promise<string> {
+        let javaHome: string;
+        // First try getting javaHome from config
+        javaHome = await this.getJavaHomeFromConfig();
+
+        // If config doesn't have javaHome, try getting from System Variables
+        if (!javaHome) {
+            javaHome = this.findJavaHomeFromSysVariables();
+        }
+
+        // If System Variables don't have javaHome, try to detect automatically from javac
+        if (!javaHome) {
+            javaHome = await this.autoDetectJavaHome();
+        }
+
+        // At this point, if we don't have a javaHome, we throw an error and ask user to add it themselves
+        if (!javaHome) {
+            throw SfdxError.create('scanner', 'jreSetupManager', 'NoJavaHomeFound', []);
+        }
+
+        return javaHome;
+    }
+
+    private findJavaHomeFromSysVariables(): string {
+        let javaHome = null;
+        const env = process.env;
+        for (const sysVariable of JAVA_HOME_SYSTEM_VARIABLES) {
+            javaHome = env[sysVariable];
+            if (javaHome) {
+                this.logger.trace(`Found javaHome from ${sysVariable} as ${javaHome}`);
+                return javaHome;
+            }
+        }
+        return javaHome;
+    }
+
+    private async autoDetectJavaHome(): Promise<string> {
+        return new Promise((resolve) => {
+            findJavaHome({ allowJre: true }, (err: Error, home: string) => {
+                if (err) {
+                    this.logger.trace(`Could not auto-detect Java Home: ${err.message}`);
+                    return resolve(null); // We don't want to reject here. No Java Home is handled later
+                } else {
+                    return resolve(home);
+                }
+            });
+        });
+    }
+
+    private async verifyPath(javaHome: string): Promise<void> {
+        const fileHandler = new FileHandler();
+        try {
+            await fileHandler.stats(javaHome);
+        } catch (error) {
+            throw SfdxError.create('scanner', 'jreSetupManager', 'InvalidJavaHome', [javaHome]);
+        }
+    }
+
+    private async verifyJavaVersion(javaHome: string): Promise<void> {
+        const versionOut = await this.fetchJavaVersion(javaHome);
+
+        // Version output looks like this:
+        // "openjdk version "11.0.6" 2020-01-14 LTS\nOpenJDK Runtime Environment Zulu11.37+17-CA (build 11.0.6+10-LTS)\nOpenJDK 64-Bit Server VM Zulu11.37+17-CA (build 11.0.6+10-LTS, mixed mode)\n"
+        // We want to get the "version \"11.0" part
+        // The version number could be of the format 11.0 or 1.8
+        const regex = 'version "(\\d+).(\\d+)';
+        const matchedParts = versionOut.match(regex);
+        this.logger.trace(`Version output match for pattern ${regex} is ${matchedParts}`);
+
+        // matchedParts should have three groups: "version \"11.0", "11", "0"
+        if (!matchedParts || matchedParts.length < 3) {
+            throw SfdxError.create('scanner', 'jreSetupManager', 'VersionNotFound', []);
+        }
+
+        const versionPart1 = parseInt(matchedParts[1]);
+        const versionPart2 = parseInt(matchedParts[2]);
+        const version = versionPart1 + '.' + versionPart2;
+        // We want to allow 1.8 and greater.
+        // Upto JDK8, the version scheme is 1.blah
+        // Starting JDK 9, the version scheme is 9.blah for 9, 10.blah for 10, etc.
+        // If either version part clicks, we should be good.
+        if (versionPart1 > 1 || versionPart2 > 8) {
+            this.logger.trace(`Java version found as ${version}`);
+            return;
+        }
+
+        // If we are here, version number is not what we are looking for
+        throw SfdxError.create('scanner', 'jreSetupManager', 'InvalidVersion', [version]);
+
+    }
+
+    private async fetchJavaVersion(javaHome: string): Promise<string> {
+        const javaWithFullPath = path.join(javaHome, 'bin', 'java');
+        // Run java -version and examine stderr for version
+        return new Promise((resolve, reject) => {
+            childProcess.execFile(javaWithFullPath, ['-version'], {},
+                (error, _stdout, stderr) => {
+                    if (error) {
+                        return reject(`Could not fetch Java version from path ${javaHome}. Reason: ${error.message}`);
+                    }
+                    // TODO: Mac always returns this value in stderr. Check if the behavior is the same for other OS types
+                    this.logger.trace(`Java version received as ${stderr}`);
+                    return resolve(stderr);
+                });
+        });
+    }
+
+    private async setJavaHomeInConfig(javaHome: string): Promise<void> {
+        this.config.set(JAVA_HOME_KEY, javaHome);
+        this.logger.trace(`Persisting config file with values ${this.config.values()}`);
+        await this.config.write();
+    }
+
+    private async getJavaHomeFromConfig(): Promise<string> {
+        const configExists = await this.config.exists();
+        if (configExists) {
+            const javaHomeJson = this.config.get(JAVA_HOME_KEY);
+            this.logger.trace(`javaHomeJson read from config file: ${javaHomeJson}`);
+            return javaHomeJson.valueOf() as string;
+        }
+        return null;
+    }
+}
+
+// Method that'll be invoked externally
+export async function verifyJreSetup(): Promise<string> {
+    const manager = await JreSetupManager.create({});
+    return await manager.verifyJreSetup();
+}
