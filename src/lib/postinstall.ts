53eb85f5de5ba51f0f18b0108d332ab3d4e4130e
Joshua Feingold <jfeingold@salesforce.com>
2020-04-02 14:50:13 -0500

    Fixed some linting errors.

--- src/lib/postinstall.ts
+++ src/lib/postinstall.ts
@@ -1,10 +1,19 @@
 /**
  *  Code to be evaluated after the scanner plugin is installed.
  */
-
-const JreSetupManager = require('./JreSetupManager');
-const messages = require('../../messages/postinstall');
+import JreSetupManager = require('./JreSetupManager');
+import messages = require('../../messages/postinstall');
 import {UX} from '@salesforce/command';
+
+async function verifyJava(ux: UX): Promise<void> {
+  ux.setSpinnerStatus(messages.verifyingJava);
+  // Use a try-catch block so we can surface issues as non-fatal warnings instead of errors.
+  try {
+    await JreSetupManager.verifyJreSetup();
+  } catch (e) {
+    ux.warn(e.message || e);
+  }
+}
 
 
 export async function execute(): Promise<void> {
@@ -19,16 +28,5 @@ export async function execute(): Promise<void> {
   } catch (e) {
     ux.error(messages.errorTemplate.replace('%s', e.message || e));
     ux.stopSpinner(messages.failed);
-  }
-}
-
-
-async function verifyJava(ux: UX): Promise<void> {
-  ux.setSpinnerStatus(messages.verifyingJava);
-  // Use a try-catch block so we can surface issues as non-fatal warnings instead of errors.
-  try {
-    await JreSetupManager.verifyJreSetup();
-  } catch (e) {
-    ux.warn(e.message || e);
   }
 }
