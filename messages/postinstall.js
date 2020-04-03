9e95ea853b67ca29d774c457e2ccdf5c4992fca3
Joshua Feingold <jfeingold@salesforce.com>
2020-04-03 13:22:07 -0500

    Removed all logging from the post-install script.

--- messages/postinstall.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "startingAll": "Beginning post-install scripts",
-  "verifyingJava": "Verifying Java setup.",
-  "done": "Done",
-  "failed": "Failed",
-  "errorTemplate": "Error: %s"
-};
--- src/lib/postinstall.ts
+++ src/lib/postinstall.ts
@@ -2,43 +2,13 @@
  *  Code to be evaluated after the scanner plugin is installed.
  */
 import JreSetupManager = require('./JreSetupManager');
-import messages = require('../../messages/postinstall');
-/*
-import {UX} from '@salesforce/command';
-
-async function verifyJava(ux: UX): Promise<void> {
-  ux.setSpinnerStatus(messages.verifyingJava);
-  // Use a try-catch block so we can surface issues as non-fatal warnings instead of errors.
-  try {
-    await JreSetupManager.verifyJreSetup();
-  } catch (e) {
-    ux.warn(e.message || e);
-  }
-}
-
-
-export async function execute(): Promise<void> {
-  const ux: UX = await UX.create();
-  ux.startSpinner(messages.startingAll);
-  // Run our scripts in a try-catch block so any important errors can bubble up to the top and be logged.
-  try {
-    // First, verify that Java is setup properly.
-    await verifyJava(ux);
-    // If we're here, there weren't any fatal errors, though there might have been some warnings.
-    ux.stopSpinner(messages.done);
-  } catch (e) {
-    ux.error(messages.errorTemplate.replace('%s', e.message || e));
-    ux.stopSpinner(messages.failed);
-  }
-}
- */
 
 async function verifyJava(): Promise<void> {
-  // Use a try-catch block so we can surface issues as non-fatal warnings instead of errors.
+  // Use a try-catch block since a failure here shouldn't tank the entire script.
   try {
     await JreSetupManager.verifyJreSetup();
   } catch (e) {
-    console.log('WARNING: ' + (e.message || e));
+    // Intentionally left empty, since the script can't really log anything intelligently.
   }
 }
 
@@ -48,8 +18,7 @@ export async function execute(): Promise<void> {
   try {
     // First, verify that Java is setup properly.
     await verifyJava();
-    // If we're here, there weren't any fatal errors, though there might have been some warnings.
   } catch (e) {
-    console.log(messages.errorTemplate.replace('%s', e.message || e));
+    // Intentionally left empty, but should be filled in if intelligent error handling is needed in the future.
   }
 }
