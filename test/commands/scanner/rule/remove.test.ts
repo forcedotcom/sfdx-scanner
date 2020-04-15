d62254eb578006c0b51578d5be020d6bbcf9d5fe
Joshua Feingold <jfeingold@salesforce.com>
2020-04-15 17:16:52 -0500

    @W-7434792@: Got started on tests. Only a few more to go.

--- test/commands/scanner/rule/remove.test.ts
+++ test/commands/scanner/rule/remove.test.ts
@@ -1,7 +1,8 @@
 import {expect, test} from '@salesforce/command/lib/test';
 import {Messages} from '@salesforce/core';
-import * as os from 'os';
+//import * as os from 'os';
 import {SFDX_SCANNER_PATH} from '../../../../src/Constants';
+import {ENGINE} from '../../../../src/lib/CustomRulePathManager';
 import fs = require('fs');
 import path = require('path');
 
@@ -19,22 +20,133 @@ if (fs.existsSync(path.join(SFDX_SCANNER_PATH, CUSTOM_PATH_OVERRIDE))) {
 	fs.unlinkSync(path.join(SFDX_SCANNER_PATH, CUSTOM_PATH_OVERRIDE));
 }
 
-let removeTest = test.env({PMD_CATALOG_NAME: CATALOG_OVERRIDE, CUSTOM_PATH_FILE: CUSTOM_PATH_OVERRIDE});
+// NOTE: The relative paths are relative to the root of the project instead of to the location of this file,
+// because the root is the working directory during test evaluation.
+const parentFolderForJars = path.resolve('./test/test-jars/apex');
+const pathToApexJar1 = path.resolve('./test/test-jars/apex/testjar1.jar');
+const pathToApexJar2 = path.resolve('./test/test-jars/apex/testjar2.jar');
+const pathToApexJar3 = path.resolve('./test/test-jars/apex/testjar3.jar');
+const pathToApexJar4 = path.resolve('./test/test-jars/apex/testjar4.jar');
+// For our tests, we'll include three Apex JARs.
+const customPathDescriptor = {
+	[ENGINE.PMD]: {
+		'apex': [pathToApexJar1, pathToApexJar2, pathToApexJar3]
+	}
+};
+
+let removeTest = test
+	.env({PMD_CATALOG_NAME: CATALOG_OVERRIDE, CUSTOM_PATH_FILE: CUSTOM_PATH_OVERRIDE})
+	.do(() => {
+		fs.writeFileSync(path.join(SFDX_SCANNER_PATH, CUSTOM_PATH_OVERRIDE), JSON.stringify(customPathDescriptor));
+	});
 
 describe('scanner:rule:remove', () => {
 	describe('E2E', () => {
-		describe('Test Case: Removing a single JAR', () => {
+		describe('Rule Removal', () => {
+			describe('Test Case: Removing a single PMD JAR', () => {
+				removeTest
+					.stdout()
+					.stderr()
+					// We'll wait three seconds then send in a 'y', to simulate the user confirming the request.
+					.stdin('y\n', 3000)
+					.timeout(10000)
+					.command(['scanner:rule:remove',
+						'--language', 'apex',
+						'--path', pathToApexJar1
+					])
+					.it('The specified JAR is deleted.', ctx => {
+						expect(ctx.stdout).to.contain(
+							messages.getMessage('output.resultSummary', [pathToApexJar1]),
+							'Console should report deletion.'
+						);
+						const updatedCustomPathJson = JSON.parse(fs.readFileSync(path.join(SFDX_SCANNER_PATH, CUSTOM_PATH_OVERRIDE)).toString());
+						expect(updatedCustomPathJson).to.deep.equal({
+							[ENGINE.PMD]: {
+								'apex': [pathToApexJar2, pathToApexJar3]
+							}
+						}, 'Deletion should have been persisted');
+					});
+			});
 
+			describe('Test Case: Removing multiple PMD JARs', () => {
+				removeTest
+					.stdout()
+					.stderr()
+					// We'll wait three seconds then send in a 'y', to simulate the user confirming the request.
+					.stdin('y\n', 3000)
+					.timeout(10000)
+					.command(['scanner:rule:remove',
+						'--language', 'apex',
+						'--path', [pathToApexJar1, pathToApexJar2].join(',')
+					])
+					.it('The specified JARs are deleted', ctx => {
+						expect(ctx.stdout).to.contain(
+							messages.getMessage('output.resultSummary', [[pathToApexJar1, pathToApexJar2].join(', ')]),
+							'Console should report deletion'
+						);
+						const updatedCustomPathJson = JSON.parse(fs.readFileSync(path.join(SFDX_SCANNER_PATH, CUSTOM_PATH_OVERRIDE)).toString());
+						expect(updatedCustomPathJson).to.deep.equal({
+							[ENGINE.PMD]: {
+								'apex': [pathToApexJar3]
+							}
+						}, 'Deletion should have been persisted');
+					});
+			});
+
+			describe('Test Case: Removing an entire folder of PMD JARs', () => {
+				removeTest
+					.stdout()
+					.stderr()
+					// We'll wait three seconds then send in a 'y', to simulate the user confirming the request.
+					.stdin('y\n', 3000)
+					.timeout(10000)
+					.command(['scanner:rule:remove',
+						'--language', 'apex',
+						'--path', parentFolderForJars
+					])
+					.it('All JARs in the target folder are deleted', ctx => {
+						expect(ctx.stdout).to.contain(
+							messages.getMessage('output.resultSummary', [[pathToApexJar1, pathToApexJar2, pathToApexJar3].join(', ')]),
+							'Console should report deletion'
+						);
+						const updatedCustomPathJson = JSON.parse(fs.readFileSync(path.join(SFDX_SCANNER_PATH, CUSTOM_PATH_OVERRIDE)).toString());
+						expect(updatedCustomPathJson).to.deep.equal({
+							[ENGINE.PMD]: {
+								'apex': []
+							}
+						}, 'Deletion should have been persisted');
+					});
+			});
+
+			describe('Edge Case: Provided path is not registered as a custom rule', () => {
+				removeTest
+					.stdout()
+					.stderr()
+					// We'll wait three seconds then send in a 'y', to simulate the user confirming the request.
+					.stdin('y\n', 3000)
+					.timeout(10000)
+					.command(['scanner:rule:remove',
+						'--language', 'apex',
+						'--path', pathToApexJar4
+					])
+					.it('All JARs in the target folder are deleted', ctx => {
+						expect(ctx.stderr).to.contain(messages.getMessage('errors.noMatchingPaths'), 'Should throw expected error');
+					});
+			});
 		});
 
-		describe('Test Case: Removing multiple JARs', () => {
+		describe('User prompt', () => {
+			describe('Test Case: User chooses to abort transaction instead of confirming', () => {
 
+			});
+
+			describe('Test Case: User uses --force flag to skip confirmation prompt', () => {
+
+			});
 		});
 
-		describe('Test Case: Removing an entire folder', () => {
+		describe('Validations', () => {
 
 		});
-
-
 	});
 });
