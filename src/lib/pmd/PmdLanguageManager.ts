fbce97739c617b05a2f8bdf030c90f52535ff1ff
Joshua Feingold <jfeingold35@gmail.com>
2020-06-23 13:33:17 -0500

    @W-7716156@: Added aliasing and new tests.

--- src/lib/pmd/PmdLanguageManager.ts
+++ src/lib/pmd/PmdLanguageManager.ts
@@ -2,13 +2,31 @@ import {Config} from '../util/Config';
 import {Controller} from '../../ioc.config';
 import {PmdEngine} from './PmdEngine';
 
-//import {Logger} from '@salesforce/core';
+import {Logger} from '@salesforce/core';
 import {AsyncCreatable} from '@salesforce/kit';
 
 const DEFAULT_LANGUAGES = ['apex', 'javascript'];
+const LANGUAGES_BY_ALIAS: Map<string, string> = new Map([
+	['apex', 'apex'],
+	['java', 'java'],
+	['javascript', 'javascript'],
+	['ecmascript', 'javascript'],
+	['js', 'javascript'],
+	['jsp', 'jsp'],
+	['modelica', 'modelica'],
+	['plsql', 'plsql'],
+	['pl/sql', 'plsql'],
+	['pl-sql', 'plsql'],
+	['scala', 'scala'],
+	['vf', 'visualforce'],
+	['visualforce', 'visualforce'],
+	['xml', 'xml'],
+	['pom', 'xml'],
+	['xsl', 'xml']
+]);
 
 class PmdLanguageManager extends AsyncCreatable {
-	//private logger: Logger;
+	private logger: Logger;
 	private config: Config;
 	private initialized: boolean;
 
@@ -16,21 +34,39 @@ class PmdLanguageManager extends AsyncCreatable {
 		if (this.initialized) {
 			return;
 		}
-		//this.logger = await Logger.child('PmdLanguageManager');
+		this.logger = await Logger.child('PmdLanguageManager');
 		this.config = await Controller.getConfig();
 		this.initialized = true;
+	}
+
+	private resolveLanguageAliases(aliases: string[]): string[] {
+		const resolvedLangs = [];
+		for (let i = 0; i < aliases.length; i++) {
+			const alias = aliases[i];
+			if (LANGUAGES_BY_ALIAS.has(alias.toLowerCase())) {
+				const lang = LANGUAGES_BY_ALIAS.get(alias.toLowerCase());
+				this.logger.trace(`Resolving language alias ${alias} to ${lang}`);
+				resolvedLangs.push(lang);
+			} else {
+				// TODO: Perhaps this should throw an error?
+				this.logger.trace(`No language found for alias ${alias}`);
+			}
+		}
+		return resolvedLangs;
 	}
 
 	public async getSupportedLanguages(): Promise<string[]> {
 		const pmdConfig = await this.config.getEngineConfig(PmdEngine.NAME);
 		// If the config specifies default languages, use those.
 		if (pmdConfig.supportedLanguages && pmdConfig.supportedLanguages.length > 0) {
-			return pmdConfig.supportedLanguages;
+			this.logger.trace(`Pulled languages ${pmdConfig.supportedLanguages} from Config.json`);
+			return this.resolveLanguageAliases(pmdConfig.supportedLanguages);
 		} else {
 			// Otherwise, assume they're using an old config that predates the property, and we'll set it to the default
 			// value for them.
 			pmdConfig.supportedLanguages = DEFAULT_LANGUAGES;
 			await this.config.setEngineConfig(PmdEngine.NAME, pmdConfig);
+			this.logger.trace(`Saving languages ${DEFAULT_LANGUAGES} to Config.json`);
 			return DEFAULT_LANGUAGES;
 		}
 	}
--- test/lib/pmd/PmdLanguageManager.test.ts
+++ test/lib/pmd/PmdLanguageManager.test.ts
@@ -7,72 +7,59 @@ import {PmdEngine} from '../../../src/lib/pmd/PmdEngine';
 
 describe('PmdLanguageManager', () => {
 	describe('getSupportedLanguages()', () => {
+		const fakePmdConfig: EngineConfigContent = {
+			name: "pmd",
+			targetPatterns: [
+				"**/*.cls", "**/*.java", "**/*.js", "**/*.page", "**/*.component", "**/*.xml",
+				"!**/node_modules/**", "!**/*-meta.xml"
+			]
+		};
+		let finalConfig: EngineConfigContent = null;
+
+		before(() => {
+			Sinon.createSandbox();
+			// getEngineConfig() should return the fake config that we've spoofed.
+			Sinon.stub(Config.prototype, 'getEngineConfig').withArgs(PmdEngine.NAME).returns(fakePmdConfig);
+			// setEngineConfig() should assign the config to finalConfig so we can see what it's trying to set.
+			Sinon.stub(Config.prototype, 'setEngineConfig').callsFake(async (name: string, ecc: EngineConfigContent) => {
+				finalConfig = ecc;
+			});
+		});
+
+		after(() => {
+			Sinon.restore();
+			finalConfig = null;
+		});
+
 		describe('When Config DOES specify supported languages', () => {
-			const withDefaultLanguages = {
-				name: "pmd",
-				targetPatterns: [
-					"**/*.cls", "**/*.java", "**/*.js", "**/*.page", "**/*.component", "**/*.xml",
-					"!**/node_modules/**", "!**/*-meta.xml"
-				],
+			it('Explicitly supported languages are returned, and config is NOT updated', async () => {
 				// Simulate a config that specifies Apex, JavaScript, and Java.
-				supportedLanguages: ['apex', 'javascript', 'java']
-			};
-			let finalConfig: EngineConfigContent = null;
+				fakePmdConfig.supportedLanguages = ['apex', 'javascript', 'java'];
 
-			before(() => {
-				Sinon.createSandbox();
-				// getEngineConfig should return the fake config that we've spoofed.
-				Sinon.stub(Config.prototype, 'getEngineConfig').withArgs(PmdEngine.NAME).returns(withDefaultLanguages);
-				// setEngineConfig should assign the modified config to the finalConfig variable so we can see what it's trying
-				// to set.
-				Sinon.stub(Config.prototype, 'setEngineConfig').callsFake(async (name: string, ecc: EngineConfigContent) => {
-					finalConfig = ecc
-				});
-			});
-
-			after(() => {
-				Sinon.restore();
-				finalConfig = null;
-			});
-
-			it('Explicitly supported languages are returned, and config IS NOT updated', async () => {
-				const langs: string[] = await PmdLanguageManager.getSupportedLanguages();
+				const langs = await PmdLanguageManager.getSupportedLanguages();
 				expect(langs).to.deep.equal(['apex', 'javascript', 'java'], 'Explicitly supported languages should have been returned');
+				expect(finalConfig).to.equal(null, 'Config should not have been overwritten');
+			});
+
+			it('Aliases are successfully resolved into a viable language name', async () => {
+				// Simulate a config that specifies some languages using some weird casing and aliasesspecifies Apex, JavaScript, and Java, using some weird casing and aliases.
+				fakePmdConfig.supportedLanguages = ['ApEx', 'EcMaScRiPt', 'Java', 'pl/sql'];
+
+				const langs = await PmdLanguageManager.getSupportedLanguages();
+				expect(langs).to.deep.equal(['apex', 'javascript', 'java', 'plsql'], 'Aliases to languages should have been resolved');
 				expect(finalConfig).to.equal(null, 'Config should not have been overwritten');
 			});
 		});
 
 		describe('When Config DOES NOT specify supported languages', () => {
-			const withoutDefaultLanguages = {
-				name: "pmd",
-				targetPatterns: [
-					"**/*.cls", "**/*.java", "**/*.js", "**/*.page", "**/*.component", "**/*.xml",
-					"!**/node_modules/**", "!**/*-meta.xml"
-				]
-			};
-			let finalConfig: EngineConfigContent = null;
+			it('Default supported languages are returned, and Config is updated', async () => {
+				// Simulate a config that specifies no default languages.
+				delete fakePmdConfig.supportedLanguages;
 
-			before(() => {
-				Sinon.createSandbox();
-				// getEngineConfig should return the fake config that we've spoofed.
-				Sinon.stub(Config.prototype, 'getEngineConfig').withArgs(PmdEngine.NAME).returns(withoutDefaultLanguages);
-				// setEngineConfig should assign the modified config to the finalConfig variable so we can see what it's trying
-				// to set.
-				Sinon.stub(Config.prototype, 'setEngineConfig').callsFake(async (name: string, ecc: EngineConfigContent) => {
-					finalConfig = ecc
-				});
-			});
-
-			after(() => {
-				Sinon.restore();
-				finalConfig = null;
-			});
-
-			it('Default supported languages are returned, and config IS updated', async () => {
-				const langs: string[] = await PmdLanguageManager.getSupportedLanguages();
+				const langs = await PmdLanguageManager.getSupportedLanguages();
 				expect(langs).to.deep.equal(['apex', 'javascript'], 'Default supported languages should have been returned');
 				expect(finalConfig).to.have.property('supportedLanguages');
-				expect(finalConfig.supportedLanguages).to.deep.equal(['apex', 'javascript'], 'Default languages should have been written to config.');
+				expect(finalConfig.supportedLanguages).to.deep.equal(['apex', 'javascript'], 'Default languages should have been written to config');
 			});
 		});
 	});
