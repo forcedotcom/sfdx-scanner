ebeabba60735eea14f96dd6e8773252937d7053e
Joshua Feingold <jfeingold35@gmail.com>
2020-09-11 16:57:08 -0500

    @W-8062540@: Refactored code into PathMatcher class and added unit tests.

---
+++ src/lib/util/PathMatcher.ts
@@ -0,0 +1,56 @@
+import picomatch = require('picomatch');
+
+type MatchingFunction = (string) => boolean;
+
+
+export class PathMatcher {
+	/**
+	 * Creates a MatchingFunction for the provided patterns.
+	 * @param {string[]} patterns - An array of strings that are either positive patterns (don't start with `!`), or
+	 *                              negative patterns (do start with `!`).
+	 * @returns {MatchingFunction} A function that will match the provided patterns.
+	 */
+	private static generateMatchingFunction(patterns: string[]): MatchingFunction {
+		// Picomatch matches patterns using a logical OR (i.e. it returns true if the target matches ANY provided pattern).
+		//
+		// Inclusion patterns (e.g., '**/*.js') indicate what a path is allowed to look like. We want files that match ANY
+		// inclusion pattern, and since that's consistent with the logical OR used by Picomatch, we can just create a single
+		// matcher for all inclusion patterns.
+		//
+		// Exclusion patterns (e.g., '!**/node_modules/**') indicate what a path is NOT allowed to look like. We want to
+		// exclude files UNLESS they match EVERY exclusion pattern, which is a logical AND.
+		// DeMorgan's Law states that (p && q) == !(!p || !q), so we can turn our AND into an OR by inverting every exclusion
+		// pattern and negating the result of that matcher.
+		const inclusionPatterns = [];
+		const exclusionPatterns = [];
+		patterns.forEach(p => p.startsWith('!') ? exclusionPatterns.push(p.slice(1)) : inclusionPatterns.push(p));
+
+		const inclusionMatcher = inclusionPatterns && inclusionPatterns.length ? picomatch(inclusionPatterns) : () => true;
+		const exclusionMatcher = exclusionPatterns && exclusionPatterns.length ? picomatch(exclusionPatterns) : () => false;
+
+		return (t: string): boolean => {return inclusionMatcher(t) && !exclusionMatcher(t)};
+	}
+
+	/**
+	 * Returns the subset of the provided paths match ANY of the provided positive patterns AND ALL provided negative patterns.
+	 * @param {string[]} targets - An array of paths
+	 * @param {string[]} patterns - An array of strings that are either positive patterns (don't start with `!`), or
+	 *                              negative patterns (do start with `!`).
+	 * @returns {string[]} - The subset of the target strings that match the provided patterns.
+	 */
+	public static filterPathsByPatterns(targets: string[], patterns: string[]): string[] {
+		const matcher: MatchingFunction = this.generateMatchingFunction(patterns);
+		return targets.filter(t => matcher(t));
+	}
+
+	/**
+	 * Returns true if the provided target string matches ANY of the provided positive patterns AND ALL provided negative patterns.
+	 * @param {string} target - A path.
+	 * @param {string[]} patterns - An array of strings that are either positive patterns (don't start with `!`), or
+	 *                              negative patterns (do start with `!`).
+	 * @returns {boolean}
+	 */
+	public static pathMatchesPatterns(target: string, patterns: string[]): boolean {
+		return this.generateMatchingFunction(patterns)(target);
+	}
+}
---
+++ test/lib/util/PathMatcher.test.ts
@@ -0,0 +1,145 @@
+import {expect} from 'chai';
+import {PathMatcher} from '../../../src/lib/util/PathMatcher';
+
+describe('PathMatcher', () => {
+	describe('#filterPathsByPatterns()', () => {
+		describe('Positive patterns only', () => {
+			const targets = [
+				'path/to/some/javascriptfile.js',
+				'/Users/jfeingold/code/CPQ/pkg/main/default/classes/QuoteTrigger.cls',
+				'path/to/some/javafile.java'
+			];
+			const patterns = ['**/*.js', '**/*.ts', '/Users/jfeingold/code/CPQ/**/*.cls'];
+
+			const results = PathMatcher.filterPathsByPatterns(targets, patterns);
+
+			it('INCLUDES paths matching ANY positive pattern', () => {
+				expect(results).to.include(targets[0], 'Paths that match a single positive pattern should be included');
+				expect(results).to.include(targets[1], 'Paths that match a single positive pattern should be included');
+			});
+
+			it('EXCLUDES paths matching NO positive patterns', () => {
+				expect(results).to.not.include(targets[2], 'Paths that match no positive patterns should be excluded');
+			});
+		});
+
+		describe('Negative patterns only', () => {
+			const targets = [
+				'path/to/some/javascriptfile.js',
+				'~/code/CPQ/pkg/main/default/classes/QuoteTrigger.cls',
+				'root/node_modules/SomeModule/main.js',
+				'~/code/CPQ/pkg/main/default/components/SomeComp.component-meta.xml'
+			];
+			const patterns = ['!**/node_modules/**', '!**/*.component-meta.xml'];
+
+			const results = PathMatcher.filterPathsByPatterns(targets, patterns);
+
+			it('INCLUDES paths matching EVERY negative pattern', () => {
+				expect(results).to.include(targets[0], 'Paths that match all negative patterns should be included');
+				expect(results).to.include(targets[1], 'Paths that match all negative patterns should be included');
+			});
+
+			it('EXCLUDES paths matching NOT EVERY negative pattern', () => {
+				expect(results).to.not.include(targets[2], 'Paths not matching every negative pattern should be excluded');
+				expect(results).to.not.include(targets[3], 'Paths not matching every negative pattern should be excluded');
+			});
+		});
+
+		describe('Mixed patterns', () => {
+			const targets = [
+				'path/to/a/JSScript.js',
+				'path/to/node_modules/lib/JSScript.js',
+				'path/to/QuoteTrigger.cls',
+				'/Users/jfeingold/code/CPQ/pkg/main/default/classes/QuoteTrigger.cls',
+				'path/to/SomeClass.java'
+			];
+			const patterns = [
+				'**/*.js', '!**/node_modules/**',
+				'**/*.cls', '!/Users/jfeingold/code/CPQ/pkg/**'
+			];
+
+			const results = PathMatcher.filterPathsByPatterns(targets, patterns);
+
+			it('INCLUDES paths matching ANY positive AND ALL negative patterns', () => {
+				expect(results).to.include(targets[0], 'Path wrongly excluded.');
+				expect(results).to.include(targets[2], 'Path wrongly excluded.')
+			});
+
+			it('EXCLUDES paths matching ANY positive patterns but NOT ALL negative patterns', () => {
+				expect(results).to.not.include(targets[1], 'Path wrongly included');
+				expect(results).to.not.include(targets[3], 'Path wrongly included');
+			});
+
+			it('EXCLUDES paths matching ALL negative patterns but NO positive patterns', () => {
+				expect(results).to.not.include(targets[4], 'Path wrongly included');
+			});
+		});
+	});
+
+	describe('#pathMatchesPatterns()', () => {
+		describe('Positive patterns only', () => {
+			const targets = [
+				'path/to/some/javascriptfile.js',
+				'/Users/jfeingold/code/CPQ/pkg/main/default/classes/QuoteTrigger.cls',
+				'path/to/some/javafile.java'
+			];
+			const patterns = ['**/*.js', '**/*.ts', '/Users/jfeingold/code/CPQ/**/*.cls'];
+			it('MATCHES paths matching ANY positive pattern', () => {
+				expect(PathMatcher.pathMatchesPatterns(targets[0], patterns)).to.equal(true, 'Paths matching a single positive pattern should return true.');
+				expect(PathMatcher.pathMatchesPatterns(targets[1], patterns)).to.equal(true, 'Paths matching a single positive pattern should return true.');
+			});
+
+			it('DOES NOT MATCH paths matching NO positive patterns', () => {
+				expect(PathMatcher.pathMatchesPatterns(targets[2], patterns)).to.equal(false, 'Paths not matching any positive pattern should return false.');
+			});
+		});
+
+		describe('Negative patterns only', () => {
+			const targets = [
+				'path/to/some/javascriptfile.js',
+				'~/code/CPQ/pkg/main/default/classes/QuoteTrigger.cls',
+				'root/node_modules/SomeModule/main.js',
+				'~/code/CPQ/pkg/main/default/components/SomeComp.component-meta.xml'
+			];
+			const patterns = ['!**/node_modules/**', '!**/*.component-meta.xml'];
+
+			it('MATCHES paths matching EVERY negative pattern', () => {
+				expect(PathMatcher.pathMatchesPatterns(targets[0], patterns)).to.equal(true, 'Paths that match all negative patterns should return true.');
+				expect(PathMatcher.pathMatchesPatterns(targets[1], patterns)).to.equal(true, 'Paths that match all negative patterns should return true.');
+			});
+
+			it('DOES NOT MATCH paths matching NOT EVERY negative pattern', () => {
+				expect(PathMatcher.pathMatchesPatterns(targets[2], patterns)).to.equal(false, 'Paths that match not all negative patterns should return false.');
+				expect(PathMatcher.pathMatchesPatterns(targets[3], patterns)).to.equal(false, 'Paths that match not all negative patterns should return false.');
+			});
+		});
+
+		describe('Mixed patterns', () => {
+			const targets = [
+				'path/to/a/JSScript.js',
+				'path/to/node_modules/lib/JSScript.js',
+				'path/to/QuoteTrigger.cls',
+				'/Users/jfeingold/code/CPQ/pkg/main/default/classes/QuoteTrigger.cls',
+				'path/to/SomeClass.java'
+			];
+			const patterns = [
+				'**/*.js', '!**/node_modules/**',
+				'**/*.cls', '!/Users/jfeingold/code/CPQ/pkg/**'
+			];
+
+			it('MATCHES paths matching positive and negative patterns', () => {
+				expect(PathMatcher.pathMatchesPatterns(targets[0], patterns)).to.equal(true, 'Path wrongly matched.');
+				expect(PathMatcher.pathMatchesPatterns(targets[2], patterns)).to.equal(true, 'Path wrongly matched.');
+			});
+
+			it('DOES NOT MATCH paths matching positive patterns but not negative patterns', () => {
+				expect(PathMatcher.pathMatchesPatterns(targets[1], patterns)).to.equal(false, 'Path wrongly not matched.');
+				expect(PathMatcher.pathMatchesPatterns(targets[3], patterns)).to.equal(false, 'Path wrongly not matched.');
+			});
+
+			it('DOES NOT MATCH paths matching negative patterns but not positive patterns', () => {
+				expect(PathMatcher.pathMatchesPatterns(targets[4], patterns)).to.equal(false, 'Path wrongly not matched.');
+			});
+		});
+	})
+});
