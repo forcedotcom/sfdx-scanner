5cc23dcd95f06288b8081f46a850c2466e10dc58
Jeff Bartolotta <jbartolotta@salesforce.com>
2020-09-29 19:24:28 -0700

    Fix csv output escaping issue
    Fix issues with csv output. Commas were converted to semicolons and quotes weren't escaped.
    
    Added additional escaping for XML output.
    
    Added more tests.

--- test/lib/RuleResultRecombinator.test.ts
+++ test/lib/RuleResultRecombinator.test.ts
@@ -1,12 +1,67 @@
 import {expect} from 'chai';
-import {RuleResult} from '../../src/types';
+import {RuleResult, RuleViolation} from '../../src/types';
 import {RuleResultRecombinator} from '../../src/lib/RuleResultRecombinator';
 import {OUTPUT_FORMAT} from '../../src/lib/RuleManager';
 import path = require('path');
+import * as csvParse from 'csv-parse';
+import {parseString} from 'xml2js';
 
 const sampleFile1 = path.join('Users', 'SomeUser', 'samples', 'sample-file1.js');
 const sampleFile2 = path.join('Users', 'SomeUser', 'samples', 'sample-file2.js');
 const sampleFile3 = path.join('Users', 'SomeUser', 'samples', 'sample-file3.java');
+const sampleFile4 = path.join('Users', 'SomeUser', 'samples', 'file-with-&.js');
+
+const edgeCaseResults: RuleResult[] = [
+	{
+		engine: 'eslint',
+		fileName: sampleFile4,
+		violations: [{
+			"line": 2,
+			"column": 11,
+			"severity": 2,
+			"message": "message with newline \n",
+			"ruleName": "Rulename with newline \n",
+			"category": "Category with newline \n",
+			"url": "https://eslint.org/docs/rules/no-unused-vars"
+		},
+		{
+			"line": 2,
+			"column": 11,
+			"severity": 2,
+			"message": "messsage with comma ,",
+			"ruleName": "Rulename with comma ,",
+			"category": "Category with comma ,",
+			"url": "https://eslint.org/docs/rules/no-unused-vars?val=one,two"
+		},
+		{
+			"line": 2,
+			"column": 11,
+			"severity": 2,
+			"message": "message with quote \"",
+			"ruleName": "Rulename with quote \"",
+			"category": "Category with quote \"",
+			"url": "https://eslint.org/docs/rules/no-unused-vars"
+		},
+		{
+			"line": 2,
+			"column": 11,
+			"severity": 2,
+			"message": "message with ampersand &",
+			"ruleName": "Rulename with ampersand &",
+			"category": "Category with ampersand &",
+			"url": "https://eslint.org/docs/rules/no-unused-vars?foo1=bar1&foo2=bar2"
+		},
+		{
+			"line": 2,
+			"column": 11,
+			"severity": 2,
+			"message": "message with >",
+			"ruleName": "Rulename with >",
+			"category": "Category with >",
+			"url": "https://eslint.org/docs/rules/no-unused-vars"
+		}]
+	}
+]
 
 const allFakeRuleResults: RuleResult[] = [
 	{
@@ -16,7 +71,7 @@ const allFakeRuleResults: RuleResult[] = [
 			"line": 2,
 			"column": 11,
 			"severity": 2,
-			"message": "'unusedParam' is defined but never used.",
+			"message": "'unusedParam1' is defined but never used.",
 			"ruleName": "no-unused-vars",
 			"category": "Variables",
 			"url": "https://eslint.org/docs/rules/no-unused-vars"
@@ -29,7 +84,7 @@ const allFakeRuleResults: RuleResult[] = [
 			"line": 4,
 			"column": 11,
 			"severity": 1,
-			"message": "'unusedParam' is defined but never used.",
+			"message": "'unusedParam2' is defined but never used.",
 			"ruleName": "no-unused-vars",
 			"category": "Variables",
 			"url": "https://eslint.org/docs/rules/no-unused-vars"
@@ -84,12 +139,27 @@ function isString(x: string | {columns; rows}): x is string {
 	return typeof x === 'string';
 }
 
+function validateJson(ruleResult: RuleResult, expectedResults: RuleResult[], expectedRuleResultIndex: number, expectedViolationIndex: number, trimMessage: boolean): void {
+	const expectedRuleResult = expectedResults[expectedRuleResultIndex];
+	const expectedViolation: RuleViolation = expectedRuleResult.violations[expectedViolationIndex];
+	const violation: RuleViolation = ruleResult.violations[expectedViolationIndex];
+	expect(ruleResult.fileName).to.equal(expectedRuleResult.fileName, 'Filename');
+	expect(ruleResult.engine).to.equal(expectedRuleResult.engine, 'Engine');
+	expect(violation.severity).to.equal(expectedViolation.severity, 'Severity');
+	expect(violation.line).to.equal(expectedViolation.line, 'Line');
+	expect(violation.column).to.equal(expectedViolation.column, 'Column');
+	expect(violation.ruleName).to.equal(expectedViolation.ruleName, 'Rule Name');
+	const expectedMessage = trimMessage ? expectedViolation.message.trim() : expectedViolation.message;
+	expect(violation.message).to.equal(expectedMessage);
+	expect(violation.url).to.equal(expectedViolation.url, 'Url');
+	expect(violation.category).to.equal(expectedViolation.category, 'Category');
+}
 
 describe('RuleResultRecombinator', () => {
 	describe('#recombineAndReformatResults()', () => {
 		describe('Output Format: JUnit', () => {
 			// This is a function for validating JUnit-formatted XMLs.
-			function validateJUnitFormatting(lines: string[], fileNames: string[], violationCounts: number[]) {
+			function validateJUnitFormatting(lines: string[], fileNames: string[], violationCounts: number[]): void {
 				expect(fileNames.length).to.equal(violationCounts.length,
 					`Improperly constructed rule. Supplied ${fileNames.length} names and ${violationCounts.length} violation counts`
 				);
@@ -184,11 +254,135 @@ describe('RuleResultRecombinator', () => {
 		});
 
 		describe('Output Format: JSON', () => {
-			// TODO: IMPLEMENT THESE TESTS
+			const messageIsTrimmed = false;
+			it ('Happy Path', async () => {
+				const {minSev, results} = await RuleResultRecombinator.recombineAndReformatResults(allFakeRuleResults, OUTPUT_FORMAT.JSON);
+				const ruleResults: RuleResult[] = JSON.parse(results as string);
+				expect(ruleResults).to.have.lengthOf(3, 'Rule Results');
+
+				// Validate each of the problem rows
+				let rrIndex = 0;
+				allFakeRuleResults.forEach(rr => {
+					let vIndex = 0;
+					rr.violations.forEach(() => {
+						validateJson(ruleResults[rrIndex], allFakeRuleResults, rrIndex, vIndex, messageIsTrimmed);
+						vIndex++;
+					});
+					rrIndex++
+				});
+
+				// Make sure we have iterated through all of the results.
+				// It's one more than the number of RuleResults because of the post increment.
+				expect(rrIndex).to.equal(3, 'Rule Result Index');
+
+				expect(minSev).to.equal(1, 'Most severe problem');
+			});
+
+			it ('Edge Cases', async () => {
+				const results = await (await RuleResultRecombinator.recombineAndReformatResults(edgeCaseResults, OUTPUT_FORMAT.JSON)).results;
+				const ruleResults: RuleResult[] = JSON.parse(results as string);
+				expect(ruleResults).to.have.lengthOf(1, 'Rule Results');
+
+				// Validate each of the problem rows
+				let rrIndex = 0;
+				edgeCaseResults.forEach(rr => {
+					let vIndex = 0;
+					rr.violations.forEach(() => {
+						validateJson(ruleResults[rrIndex], edgeCaseResults, rrIndex, vIndex, messageIsTrimmed);
+						vIndex++;
+					});
+					rrIndex++
+				});
+
+				// Make sure we have iterated through all of the results.
+				// It's one more than the number of RuleResults because of the post increment.
+				expect(rrIndex).to.equal(1, 'RuleResult Index');
+			});
 		});
 
 		describe('Output Format: XML', () => {
-			// TODO: IMPLEMENT THESE TESTS
+			const messageIsTrimmed = true;
+			async function convertXmlToJson(results: string): Promise<RuleResult[]> {
+				const parsedXml = await new Promise((resolve, reject) => {
+					parseString(results as string, (err, output) => {
+						if (err) {
+							reject(err);
+						}
+						resolve(output);
+					});
+				});
+				const records = parsedXml['results']['result'];
+
+				const ruleResults: RuleResult[] = [];
+				records.forEach(record => {
+					const violations = [];
+
+					record['violation'].forEach(v => {
+						violations.push({
+							severity: parseInt(v['$']['severity']),
+							line: parseInt(v['$']['line']),
+							column: parseInt(v['$']['column']),
+							ruleName: v['$']['rule'],
+							message: v['_'],
+							url: v['$']['url'],
+							category: v['$']['category']
+						})
+					});
+
+					const ruleResult: RuleResult = {
+						engine: record['$']['engine'],
+						fileName: record['$']['file'],
+						violations: violations
+					};
+					ruleResults.push(ruleResult);
+				});
+
+				return ruleResults;
+			}
+
+			it ('Happy Path', async () => {
+				const {minSev, results} = await RuleResultRecombinator.recombineAndReformatResults(allFakeRuleResults, OUTPUT_FORMAT.XML);
+				const ruleResults: RuleResult[] = await convertXmlToJson(results as string);
+				expect(ruleResults).to.have.lengthOf(3, 'Rule Results');
+
+				// Validate each of the problem rows
+				let rrIndex = 0;
+				allFakeRuleResults.forEach(rr => {
+					let vIndex = 0;
+					rr.violations.forEach(() => {
+						validateJson(ruleResults[rrIndex], allFakeRuleResults, rrIndex, vIndex, messageIsTrimmed);
+						vIndex++;
+					});
+					rrIndex++
+				});
+
+				// Make sure we have iterated through all of the results.
+				// It's one more than the number of RuleResults because of the post increment.
+				expect(rrIndex).to.equal(3, 'Rule Result Index');
+
+				expect(minSev).to.equal(1, 'Most severe problem');
+			});
+
+			it ('Edge Cases', async () => {
+				const results =  (await RuleResultRecombinator.recombineAndReformatResults(edgeCaseResults, OUTPUT_FORMAT.XML)).results;
+				const ruleResults: RuleResult[] = await convertXmlToJson(results as string);
+				expect(ruleResults).to.have.lengthOf(1, 'Rule Results');
+
+				// Validate each of the problem rows
+				let rrIndex = 0;
+				edgeCaseResults.forEach(rr => {
+					let vIndex = 0;
+					rr.violations.forEach(() => {
+						validateJson(ruleResults[rrIndex], edgeCaseResults, rrIndex, vIndex, messageIsTrimmed);
+						vIndex++;
+					});
+					rrIndex++
+				});
+
+				// Make sure we have iterated through all of the results.
+				// It's one more than the number of RuleResults because of the post increment.
+				expect(rrIndex).to.equal(1, 'RuleResult Index');
+			});
 		});
 
 		describe('Output Format: Table', () => {
@@ -196,7 +390,92 @@ describe('RuleResultRecombinator', () => {
 		});
 
 		describe('Output Format: CSV', () => {
+			function validateCsvRow(violation, expectedResults: RuleResult[], expectedRuleResultIndex: number, expectedViolationIndex: number, expectedProblemNumber: number): void {
+				const expectedRuleResult = expectedResults[expectedRuleResultIndex];
+				const expectedViolation = expectedRuleResult.violations[expectedViolationIndex];
+				expect(violation[0]).to.equal(`${expectedProblemNumber}`, 'Problem number');
+				expect(violation[1]).to.equal(expectedRuleResult.fileName, 'Filename');
+				expect(violation[2]).to.equal(`${expectedViolation.severity}`, 'Severity');
+				expect(violation[3]).to.equal(`${expectedViolation.line}`, 'Line');
+				expect(violation[4]).to.equal(`${expectedViolation.column}`, 'Column');
+				expect(violation[5]).to.equal(expectedViolation.ruleName, 'Rule Name');
+				// The message is trimmed before converting to CSV
+				expect(violation[6]).to.equal(expectedViolation.message.trim(), 'Message');
+				expect(violation[7]).to.equal(expectedViolation.url, 'Url');
+				expect(violation[8]).to.equal(expectedViolation.category, 'Category');
+				expect(violation[9]).to.equal(expectedRuleResult.engine, 'Engine');
+			}
 
+			it ('Happy Path', async () => {
+				const {minSev, results} = await RuleResultRecombinator.recombineAndReformatResults(allFakeRuleResults, OUTPUT_FORMAT.CSV);
+				const records = await new Promise((resolve, reject) => {
+					csvParse(results as string, (err, output) => {
+						if (err) {
+							reject(err);
+						}
+						resolve(output);
+					});
+				});
+				expect(records).to.have.lengthOf(7, 'Expected allFakeRuleResults violations plus the header');
+
+				// Validate the header
+				const header = records[0];
+				// TODO: More validation
+				expect(header[0]).to.equal('Problem');
+
+				// Validate each of the problem rows
+				let rrIndex = 0;
+				let problemNumber = 1;
+				allFakeRuleResults.forEach(rr => {
+					let vIndex = 0;
+					rr.violations.forEach(() => {
+						validateCsvRow(records[problemNumber], allFakeRuleResults, rrIndex, vIndex, problemNumber);
+						vIndex++;
+						problemNumber++;
+					});
+					rrIndex++
+				});
+
+				// Make sure we have iterated through all of the results.
+				// It's one more than the number of problems because of the post increment.
+				expect(problemNumber).to.equal(7, 'Problem Number Index');
+
+				expect(minSev).to.equal(1, 'Most severe problem');
+			});
+
+			it ('Edge Cases', async () => {
+				const results = (await RuleResultRecombinator.recombineAndReformatResults(edgeCaseResults, OUTPUT_FORMAT.CSV)).results;
+				const records = await new Promise((resolve, reject) => {
+					csvParse(results as string, (err, output) => {
+						if (err) {
+							reject(err);
+						}
+						resolve(output);
+					});
+				});
+				expect(records).to.have.lengthOf(6, 'Expected edgeCaseResults violations plus the header');
+
+				// Validate the header
+				const header = records[0];
+				expect(header[0]).to.equal('Problem');
+
+				// Validate each of the problem rows
+				let rrIndex = 0;
+				let problemNumber = 1;
+				edgeCaseResults.forEach(rr => {
+					let vIndex = 0;
+					rr.violations.forEach(() => {
+						validateCsvRow(records[problemNumber], edgeCaseResults, rrIndex, vIndex, problemNumber);
+						vIndex++;
+						problemNumber++;
+					});
+					rrIndex++
+				});
+
+				// Make sure we have iterated through all of the results.
+				// It's one more than the number of problems because of the post increment.
+				expect(problemNumber).to.equal(6, 'Problem Number Index');
+			});
 		});
 	});
 });
