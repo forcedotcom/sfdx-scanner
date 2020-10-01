67df4767739b99cb7b1adc977cd968ee66ad5076
Joshua Feingold <jfeingold35@gmail.com>
2020-10-01 18:08:07 -0500

    @W-7983101@: First pass at duplicating files into a temporary directory for use in RetireJS.

--- src/lib/retire-js/RetireJsEngine.ts
+++ src/lib/retire-js/RetireJsEngine.ts
@@ -4,8 +4,10 @@ import {Config} from '../util/Config';
 import {RuleEngine} from '../services/RuleEngine';
 import {Catalog, Rule, RuleGroup, RuleResult, RuleTarget} from '../../types';
 import {ENGINE} from '../../Constants';
+import {FileHandler} from '../util/FileHandler';
 //import childProcess = require('child_process');
-//import path = require('path');
+import path = require('path');
+import fs = require('fs');
 
 // Unlike the other engines we use, RetireJS doesn't really have "rules" per se. So we sorta have to synthesize a
 // "catalog" out of RetireJS's normal behavior.
@@ -35,7 +37,9 @@ export class RetireJsEngine implements RuleEngine {
 	// RetireJS isn't really built to be invoked programmatically, so we'll need to invoke it as a CLI command. However, we
 	// can't assume that they have the module installed globally. So what we're doing here is identifying the path to the
 	// locally-scoped `retire` module, and then using that to derive a path to the CLI-executable JS script.
-	//private static RETIRE_JS_PATH: string = require.resolve('retire').replace(path.join('lib', 'retire.js'), path.join('bin', 'retire'));
+	private static RETIRE_JS_PATH: string = require.resolve('retire').replace(path.join('lib', 'retire.js'), path.join('bin', 'retire'));
+
+	private pathAliasMap: Map<string, string> = new Map();
 
 	private logger: Logger;
 	private config: Config;
@@ -56,6 +60,13 @@ export class RetireJsEngine implements RuleEngine {
 	// TODO: We need to actually implement this method.
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/require-await, no-unused-vars
 	public async run(ruleGroups: RuleGroup[], rules: Rule[], target: RuleTarget[], engineOptions: Map<string, string>): Promise<RuleResult[]> {
+		console.log(`Identified RetireJS exe path as ${RetireJsEngine.RETIRE_JS_PATH}`);
+		console.log(`Targets are: ${JSON.stringify(target)}`);
+
+		// RetireJS doesn't accept individual files. It only accepts directories. So we need to resolve all of the files
+		// we were given into a directory that we can pass into RetireJS.
+		const tmpDir = await this.createTmpDirWithDuplicatedTargets(target);
+		console.log(`Created parent directory ${tmpDir}`);
 		return [];
 	}
 
@@ -75,5 +86,54 @@ export class RetireJsEngine implements RuleEngine {
 
 	public async isEnabled(): Promise<boolean> {
 		return await this.config.isEngineEnabled(RetireJsEngine.ENGINE_ENUM);
+	}
+
+	private async createIntermediateDir(dir): Promise<void> {
+		return new Promise((res, rej) => {
+			fs.mkdir(dir, (err) => {
+				if (err) {
+					rej(err);
+				} else {
+					res();
+				}
+			});
+		});
+	}
+
+	private async createTmpDirWithDuplicatedTargets(targets: RuleTarget[]): Promise<any> {
+		// Create a temporary parent directory into which we'll transplant all of our target files.
+		const tmpParent: string = await new FileHandler().tmpDirWithCleanup();
+		const fileCopyPromises: Promise<void>[] = [];
+
+		// We want to duplicate all of the targeted files into our temporary directory.
+		let targetCount = 0;
+		for (const target of targets) {
+			for (const p of target.paths) {
+				// We want to make sure that every duplicate file has a unique path to avoid collision. The target paths
+				// are all absolute paths, and you can't just slam absolute paths together. So we need to get creative.
+				// We'll associate the directory portion of each path with a unique alias...
+				const dirPrefix = path.dirname(p);
+				// ...Then use the alias to create a directory below the temporary parent, unless we've already done so.
+				if (!this.pathAliasMap.has(dirPrefix)) {
+					const dirAlias = `TMP_${targetCount++}`;
+					this.pathAliasMap.set(dirPrefix, dirAlias);
+					await this.createIntermediateDir(path.join(tmpParent, dirAlias));
+				}
+				// Then we'll copy the original target file to the child directory, thereby preserving path uniqueness.
+				fileCopyPromises.push(new Promise((res, rej) => {
+					fs.copyFile(p, path.join(tmpParent, this.pathAliasMap.get(dirPrefix), path.basename(p)), (err) => {
+						if (err) {
+							rej(err);
+						} else {
+							res();
+						}
+					});
+				}));
+			}
+		}
+
+		await Promise.all(fileCopyPromises);
+		// If we successfully created all of the duplicate files, then we can return the temporary parent directory.
+		return tmpParent;
 	}
 }
