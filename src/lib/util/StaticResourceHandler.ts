b75a6f524b7d5b2f2edda834d42c441465303ad6
Joshua Feingold <jfeingold35@gmail.com>
2021-02-08 10:44:07 -0600

    @W-8841422@: RetireJS now determines file eligibility by checking contents instead of metadata MIME type.

--- src/lib/util/StaticResourceHandler.ts
+++ src/lib/util/StaticResourceHandler.ts
@@ -1,20 +1,13 @@
 import {SfdxError} from '@salesforce/core';
 import {FileHandler} from './FileHandler';
-import {ElementCompact, xml2js} from 'xml-js';
+import {isText} from 'istextorbinary';
+import isZip = require('is-zip');
 
 
 export enum StaticResourceType {
 	ZIP = 'zip',
-	JS = 'js',
+	TEXT = 'text',
 	OTHER = 'other'
-}
-
-interface StaticResourceJson extends ElementCompact  {
-	StaticResource: {
-		contentType: {
-			_text: string;
-		};
-	};
 }
 
 export class StaticResourceHandler {
@@ -25,63 +18,31 @@ export class StaticResourceHandler {
 		this.resultCache = new Map();
 	}
 
-	private isStaticResourceJson(fileContents: ElementCompact): fileContents is StaticResourceJson {
-		return !!(fileContents.StaticResource
-			&& fileContents.StaticResource.contentType
-			&& fileContents.StaticResource.contentType._text);
-
-	}
-
 	public async identifyStaticResourceType(filename: string): Promise<StaticResourceType> {
 		// If we've already cached a value for this file, we can return early.
 		if (this.resultCache.has(filename)) {
 			return this.resultCache.get(filename);
 		}
-		// Otherwise, we need to do some work.
+		// Otherwise, we need to do some work. Spin up a FileHandler and use it to get the contents of the file as a buffer.
 		const fh = new FileHandler();
-		const metafileName = filename + '-meta.xml';
-
-		// If there's no corresponding `-meta.xml` file, we should assume that this isn't a static resource at all.
-		// Rather than throwing an error, we'll just return null. That way, upstream callers can decide whether to throw
-		// an error.
-		if (!await fh.exists(metafileName)) {
-			this.resultCache.set(filename, null);
-			return null;
-		}
-		// If there's a `-meta.xml` file, we should read it and parse it into a JSON.
-		let metaContents: string = null;
-		let metaJson: ElementCompact = null;
-
+		let buffer = null;
 		try {
-			// If there's a `-meta.xml` file, we should read it and parse it into a JSON.
-			metaContents = await fh.readFile(metafileName);
-			metaJson = xml2js(metaContents, {compact: true, ignoreDeclaration: true});
+			buffer = await fh.readFileAsBuffer(filename);
 		} catch (e) {
-			// We'll throw an error here, because it's always a problem if something that looks like a static resource
-			// can't be processed.
-			throw new SfdxError(`Could not parse ${metafileName} into JSON: ${e.message || e}`);
+			// We'll throw an error here, because being unable to parse a static resource is definitely a problem that
+			// should be surfaced to the user.
+			throw new SfdxError(`Could not read ${filename}: ${e.message || e}`);
 		}
 
-		if (!this.isStaticResourceJson(metaJson)) {
-			// We'll throw an error here, because it's always a problem if something that looks like a static resource
-			// can't be processed.
-			throw new SfdxError(`${metafileName} did not match expected format`);
+		let fileType: StaticResourceType = null;
+		if (isText(null, buffer)) {
+			fileType = StaticResourceType.TEXT;
+		} else if (isZip(buffer)) {
+			fileType = StaticResourceType.ZIP;
+		} else {
+			fileType = StaticResourceType.OTHER;
 		}
-
-		// use the MIME type to determine what type of static resource we're looking at.
-		switch (metaJson.StaticResource.contentType._text) {
-			case 'application/zip':
-			case 'application/x-zip-compressed':
-				this.resultCache.set(filename, StaticResourceType.ZIP);
-				break;
-			case 'application/javascript':
-			case 'text/javascript':
-			case 'application/x-javascript':
-				this.resultCache.set(filename, StaticResourceType.JS);
-				break;
-			default:
-				this.resultCache.set(filename, StaticResourceType.OTHER);
-		}
-		return this.resultCache.get(filename);
+		this.resultCache.set(filename, fileType);
+		return fileType;
 	}
 }
