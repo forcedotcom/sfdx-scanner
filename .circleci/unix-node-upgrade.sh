6a90c76a17c47dc024e5d452ddcbdcd4ed70394f
Joshua Feingold <jfeingold35@gmail.com>
2021-07-08 13:56:02 -0500

    asdf

--- .circleci/unix-node-upgrade.sh
+++ .circleci/unix-node-upgrade.sh
@@ -30,7 +30,7 @@ grep ${DESIRED_FILE}.tar.xz SHASUMS256.txt | sha256sum -c -
 sudo tar --strip-components=2 -xJ -C /usr/local/bin/ ${DESIRED_FILE}/bin/node -f ${DESIRED_FILE}.tar.xz
 
 # Install NPM using the locally-copied install script.
-.circleci/npm-install.sh
+sudo .circleci/npm-install.sh
 
 # Delete the downloaded files.
 rm -f ${DESIRED_FILE}.tar.xz
