7f5a7c5fe12cd88b6ed682901cfcbb0bcaf2c6f7
Roopa Mohan <roopidevs@gmail.com>
2022-06-13 11:56:38 -0700

    Removing settings.gradle on subprojects to keep up with gradle standards of having a single root in a nested system

--- pmd-cataloger/settings.gradle.kts
+++
@@ -1,5 +0,0 @@
-rootProject.name = "pmd-cataloger"
-
-include(":cli-messaging")
-project(":cli-messaging").projectDir = file("../cli-messaging")
-
--- sfge/settings.gradle.kts
+++
@@ -1,4 +0,0 @@
-rootProject.name = "sfge"
-
-include(":cli-messaging")
-project(":cli-messaging").projectDir = file("../cli-messaging")
