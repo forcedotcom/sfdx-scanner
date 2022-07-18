97782e3231e08a9eecf97ca960a56ea02be4a497
Roopa Mohan <roopidevs@gmail.com>
2022-07-18 16:15:20 -0700

    @W-11397711@: Fixing minor typo

--- messages/common.js
+++ messages/common.js
@@ -1,3 +1,3 @@
 module.exports = {
-	FEEDBACK_SURVEY_BANNER: `We're constantly improving Salesforce Code Analayzer. Tell us what you think! Give feedback at https://research.net/r/SalesforceCA.`
+	FEEDBACK_SURVEY_BANNER: `We're constantly improving Salesforce Code Analyzer. Tell us what you think! Give feedback at https://research.net/r/SalesforceCA.`
 };
