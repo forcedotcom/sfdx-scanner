51284f4b7527aeeb457c1e359dcd3deed415dd48
Roopa Mohan <roopidevs@gmail.com>
2022-07-26 11:26:48 -0700

    Removing unused file

--- src/lib/util/CommonCommandUtils.ts
+++
@@ -1,9 +0,0 @@
-import { SfdxCommand } from "@salesforce/command";
-import {Messages} from '@salesforce/core';
-
-// Initialize Messages with the current plugin directory
-Messages.importMessagesDirectory(__dirname);
-const commonMessages = Messages.loadMessages('@salesforce/sfdx-scanner', 'common');
-
-
-
