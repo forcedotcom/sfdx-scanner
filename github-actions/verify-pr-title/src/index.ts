dfabc36fb2a28841fcd02a8546bf07a009e016b9
Joshua Feingold <jfeingold35@gmail.com>
2022-11-08 09:44:06 -0600

    asdf

--- github-actions/verify-pr-title/src/index.ts
+++ github-actions/verify-pr-title/src/index.ts
@@ -19,6 +19,7 @@ function run(): void {
 
 		// Examine the title for the expected patterns
 		const title = pullRequest.title;
+		console.log(`Keys are ${Object.keys(pullRequest)}`);
 		if (verifyPRTitleForBugId(title) && verifyPRTitleForBadTitle(title)) {
 			console.log(`PR Title '${title}' accepted.`);
 		} else {
