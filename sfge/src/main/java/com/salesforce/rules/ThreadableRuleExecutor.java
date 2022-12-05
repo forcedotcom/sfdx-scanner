7e3b5f2de3ef422524633e612ed940c00012b046
Joshua Feingold <jfeingold35@gmail.com>
2022-12-05 14:09:24 -0600

    @W-11988825@: Increased number of captured frames from 3 to 6.

--- sfge/src/main/java/com/salesforce/rules/ThreadableRuleExecutor.java
+++ sfge/src/main/java/com/salesforce/rules/ThreadableRuleExecutor.java
@@ -184,10 +184,13 @@ public class ThreadableRuleExecutor {
                     }
                 } else {
                     // If the thread threw another exception, wrap it in a violation, including the
-                    // last 3 frames that got executed.
+                    // last 6 frames that got executed.
+                    // NOTE: The choice of 6 frames is arbitrary. If users complain, we should feel free to adjust
+                    //       it downwards. By contrast, if we find it to be insufficiently informative, we should
+                    //       feel free to adjust it upwards.
                     String frameString =
                             Arrays.stream(ex.getStackTrace())
-                                    .limit(3)
+                                    .limit(6)
                                     .map(StackTraceElement::toString)
                                     .collect(Collectors.joining(";"));
                     final String details =
