ebadf4a75a99b5f4af61aab386751ad9ab2a068e
Joshua Feingold <jfeingold35@gmail.com>
2023-01-03 17:34:01 -0600

    Adding better tests.

--- github-actions/summarize-errors/test/summarizeJUnitErrors.test.ts
+++ github-actions/summarize-errors/test/summarizeJUnitErrors.test.ts
@@ -1,10 +1,27 @@
+import path = require('path');
 import { expect } from "chai";
 import {summarizeErrors} from "../lib/summarizeJUnitErrors";
 
 describe("#summarizeErrors", () => {
-	it('does the thing', async () => {
-		const t = await summarizeErrors("/Users/jfeingold/Code/sfdx-scanner/sfge");
-		console.log(t);
-		expect(t).to.exist;
+	it('When there are no failures, does nothing', async () => {
+		const pathToProject = path.resolve(path.join('code-fixtures', 'no-failures'));
+		const summary: string[] = await summarizeErrors(pathToProject);
+		expect(summary.length).to.equal(0);
+	});
+
+	it('If one file has failures, one summary is added', async () => {
+		const pathToProject = path.resolve(path.join('code-fixtures', 'one-failing-file'));
+		const summary: string[] = await summarizeErrors(pathToProject);
+		expect(summary.length).to.equal(1);
+		expect(summary[0]).to.contain("properlyRunsStaticRule()");
+	});
+
+	it('When multiple files have failures, one summary per file is added', async () => {
+		const pathToProject = path.resolve(path.join('code-fixtures', 'multiple-failing-files'));
+		const summary: string[] = await summarizeErrors(pathToProject);
+		expect(summary.length).to.equal(2);
+		expect(summary[0]).to.contain("properlyRunsStaticRule()");
+		expect(summary[1]).to.contain("testObjectLevelCheckIsntAccepted(FlsValidationType, String, AbstractPathBasedRule, String): READ");
+		expect(summary[1]).to.contain("testObjectLevelCheckIsntAccepted(FlsValidationType, String, AbstractPathBasedRule, String): UPDATE");
 	});
 });
