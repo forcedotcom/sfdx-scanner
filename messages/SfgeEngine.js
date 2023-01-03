a221a42aa8f138b75dd28ea179e34e7ffcd70dc5
Joshua Feingold <jfeingold35@gmail.com>
2023-01-03 12:48:39 -0600

    @W-11533657@: Typo in message.

--- messages/SfgeEngine.js
+++ messages/SfgeEngine.js
@@ -4,6 +4,6 @@ module.exports = {
 		"spinnerStart": "Analyzing with Salesforce Graph Engine. See %s for details."
 	},
 	"errors": {
-		"failedWithoutProjectDir": `The --projectdir/-p flag is missing. Rerun your command with --projectdir/-p to allow Graph Engine to run, or with --engine/-e to exclude Graph Engine from execution.`
+		"failedWithoutProjectDir": `The --projectdir|-p flag is missing. Rerun your command with --projectdir|-p to allow Graph Engine to run, or with --engine|-e to exclude Graph Engine from execution.`
 	}
 };
