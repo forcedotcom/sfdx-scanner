c5fed3b23616febbc6a22d985b698351c54b6f46
Joshua Feingold <jfeingold35@gmail.com>
2023-02-28 11:57:15 -0600

    @W-12494075@: Added more commenting.

--- sfge/src/main/java/com/salesforce/rules/AbstractPathBasedRule.java
+++ sfge/src/main/java/com/salesforce/rules/AbstractPathBasedRule.java
@@ -2,6 +2,7 @@ package com.salesforce.rules;
 
 /**
  * Abstract parent class for rules whose execution requires the construction and/or traversal of
- * {@link com.salesforce.graph.ApexPath} instances.
+ * {@link com.salesforce.graph.ApexPath} instances. Currently, this class has no methods, but exists
+ * because it's beneficial to have all path-based rules share a common parent class.
  */
 public abstract class AbstractPathBasedRule extends AbstractRule {}
