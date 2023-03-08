8647ae08ddf97552b0b4ce3d32dd2d084abf4c69
Joshua Feingold <jfeingold35@gmail.com>
2023-03-08 13:30:55 -0600

    @W-11464344@: UnusedMethodRule tests further refactored and enhanced.

--- sfge/src/test/java/com/salesforce/rules/unusedmethod/CollidingNamesTest.java
+++ sfge/src/test/java/com/salesforce/rules/unusedmethod/CollidingNamesTest.java
@@ -4,8 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 /**
  * There are a bunch of weird edge cases that can happen when class names, variable names, and
@@ -13,101 +11,6 @@ import org.junit.jupiter.params.provider.ValueSource;
  * their own file.
  */
 public class CollidingNamesTest extends BaseUnusedMethodTest {
-    /**
-     * If an outer class has a static method, and its inner class has an instance method with the
-     * same name, then invoking that method without the `this` keyword should still count as using
-     * the instance method, not the static method.
-     */
-    @Test
-    @Disabled
-    public void innerInstanceOverlapsWithOuterStatic_expectViolationForOuter() {
-        String[] sourceCodes = {
-            "global virtual class ParentClass {"
-                    // Declare a static method on the outer class with a certain name.
-                    + "    public static boolean overlappingName() {\n"
-                    + "        return true;\n"
-                    + "    }\n"
-                    + "    global class InnerOfParent {\n"
-                    // Declare an instance method on the inner class with the same name.
-                    + "        public boolean overlappingName() {\n"
-                    + "            return true;\n"
-                    + "        }\n"
-                    // Use the engine directive to prevent this method from tripping the rule.
-                    + "        /* sfge-disable-stack UnusedMethodRule */\n"
-                    + "        public boolean invoker() {"
-                    // Invoke the instance method without using `this`.
-                    + "            return overlappingName();\n"
-                    + "        }\n"
-                    + "    }\n"
-                    + "}\n",
-            // Declare a class that extends the parent class, thereby inheriting its static method.
-            "global class ChildClass extends ParentClass {\n"
-                    + "    global class InnerOfChild {\n"
-                    // Declare an instance method on the inner class with the same name.
-                    + "        public boolean overlappingName() {\n"
-                    + "            return true;\n"
-                    + "        }\n"
-                    // Use the engine directive to prevent this method from tripping the rule.
-                    + "        /* sfge-disable-stack UnusedMethodRule */\n"
-                    + "        public boolean invoker() {\n"
-                    // Invoke the instance method without using `this`.
-                    + "            return overlappingName();\n"
-                    + "        }\n"
-                    + "    }\n"
-                    + "}\n"
-        };
-        // We expect the outer static method to be unused, and both inner methods to be used.
-        assertViolations(
-                sourceCodes,
-                v -> {
-                    assertEquals("overlappingName", v.getSourceVertexName());
-                    assertEquals("ParentClass", v.getSourceVertex().getDefiningType());
-                });
-    }
-
-    /**
-     * If an inner class extends another class and inherits a method whose name matches a static
-     * method on the outer class, then invoking the method in the inner class without the `this`
-     * keyword should still count as an invocation of the inherited method, not the outer static
-     * one.
-     */
-    @ValueSource(strings = {"public", "public static"})
-    @ParameterizedTest(name = "{displayName}: parent method {0}")
-    @Disabled
-    public void inheritedInnerClassOverlapsWithOuter_expectViolations(String scope) {
-        String[] sourceCodes = {
-            // Declare a parent class with a method.
-            "public virtual class ParentClass {\n"
-                    + String.format("    %s boolMethod() {\n", scope)
-                    + "        return true;\n"
-                    + "    }\n"
-                    + "}\n",
-            // Declare an outer class with a static method whose name matches the parent class's
-            // method.
-            "public class OuterClass {\n"
-                    + "    public static boolean boolMethod() {\n"
-                    + "        return false;\n"
-                    + "    }\n"
-                    // Declare an inner class that extends the parent class,
-                    // thereby inheriting its methods.
-                    + "    public class InnerChild extends ParentClass {\n"
-                    // Give the inner class a method that calls the inherited method.
-                    + "        /* sfge-disable-stack UnusedMethodRule */\n"
-                    + "        public boolean invoker() {\n"
-                    + "            return boolMethod();\n"
-                    + "        }\n"
-                    + "    }\n"
-                    + "}\n"
-        };
-        // We expect the inherited method on the parent class to be called, not the static
-        // method on the outer class.
-        assertViolations(
-                sourceCodes,
-                v -> {
-                    assertEquals("boolMethod", v.getSourceVertexName());
-                    assertEquals("OuterClass", v.getSourceVertex().getDefiningType());
-                });
-    }
 
     /**
      * If an outer class defines an inner class, it can reference the class with just the inner
@@ -115,40 +18,43 @@ public class CollidingNamesTest extends BaseUnusedMethodTest {
      * name. In this case, the methods invoked are the ones on the inner class.
      */
     @Test
+    // TODO: INSTEAD OF SIMPLY ENABLING THIS TEST, CONSIDER MOVING IT TO InstanceMethodsTest.java!
     @Disabled
     public void innerClassNameOverlapsWithOuter_expectViolations() {
+        // spotless:off
         String[] sourceCodes = {
             "global class OuterClass {\n"
-                    + "    global class OverlappingNameClass {\n"
-                    // Declare a constructor.
-                    + "        public OverlappingNameClass(boolean b) {\n"
-                    + "        }\n"
-                    // Declare an instance method.
-                    + "        public boolean someMethod() {\n"
-                    + "            return true;\n"
-                    + "        }\n"
-                    + "    }\n"
-                    // Use the engine directive to prevent this method from tripping the rule.
-                    + "    /* sfge-disable-stack UnusedMethodRule */\n"
-                    + "    public boolean invokerMethod() {\n"
-                    // Invoke the constructor.
-                    + "        OverlappingNameClass instance = new OverlappingNameClass(true);\n"
-                    // Invoke the instance method.
-                    + "        return instance.someMethod();\n"
-                    + "    }\n"
-                    + "}\n",
+          + "    global class OverlappingNameClass {\n"
+            // Declare a constructor.
+          + "        public OverlappingNameClass(boolean b) {\n"
+          + "        }\n"
+            // Declare an instance method.
+          + "        public boolean someMethod() {\n"
+          + "            return true;\n"
+          + "        }\n"
+          + "    }\n"
+            // Use the engine directive to prevent this method from tripping the rule.
+          + "    /* sfge-disable-stack UnusedMethodRule */\n"
+          + "    public boolean invokerMethod() {\n"
+            // Invoke the constructor.
+          + "        OverlappingNameClass instance = new OverlappingNameClass(true);\n"
+            // Invoke the instance method.
+          + "        return instance.someMethod();\n"
+          + "    }\n"
+          + "}\n",
             // Declare another class with the same name as the other class's inner class.
             "global class OverlappingNameClass {\n"
-                    // Give it a constructor with the same signature as the inner class.
-                    + "    public OverlappingNameClass(boolaen b) {\n"
-                    + "    }\n"
-                    // Give it a method with the same signature as the instance method on the inner
-                    // class.
-                    + "    public boolean someMethod() {\n"
-                    + "        return true;\n"
-                    + "    }\n"
-                    + "}\n"
+            // Give it a constructor with the same signature as the inner class.
+          + "    public OverlappingNameClass(boolaen b) {\n"
+          + "    }\n"
+            // Give it a method with the same signature as the instance method on the inner
+            // class.
+          + "    public boolean someMethod() {\n"
+          + "        return true;\n"
+          + "    }\n"
+          + "}\n"
         };
+        // spotless:on
         // All methods on the outer class should be unused.
         assertViolations(
                 sourceCodes,
@@ -161,49 +67,6 @@ public class CollidingNamesTest extends BaseUnusedMethodTest {
                     assertEquals("someMethod", v.getSourceVertexName());
                     assertEquals("OverlappingNameClass", v.getSourceDefiningType());
                     assertEquals(4, v.getSourceLineNumber());
-                });
-    }
-
-    /**
-     * If a variable shares the same name as a wholly unrelated class, and it has an instance method
-     * whose name overlaps with that of a static method on that other class, then calling
-     * `var.theMethod()` invokes the instance method, not the static one. So the static method
-     * should count as unused.
-     */
-    @Test
-    @Disabled
-    public void variableSharesNameWithOtherClass_expectViolation() {
-        String[] sourceCodes = {
-            "global class MyClass {\n"
-                    // Declare a static method.
-                    + "    public static boolean someMethod() {\n"
-                    + "        return true;\n"
-                    + "    }\n"
-                    + "}\n",
-            "global class MyOtherClass {\n"
-                    // Declare an instance method with the same name as the
-                    // other class's static method.
-                    + "    public boolean someMethod() {\n"
-                    + "        return false;\n"
-                    + "    }\n"
-                    + "}\n",
-            "global class InvokerClass {\n"
-                    // Use the engine directive to prevent this method from tripping the rule.
-                    + "    /* sfge-disable-stack UnusedMethodRule */\n"
-                    // This method's param parameter is an instance of MyOtherClass
-                    // whose name is myClass.
-                    + "    public boolean invokerMethod(MyOtherClass myClass) {\n"
-                    // Per manual experimentation, this counts as an invocation of the
-                    // instance method, NOT the static method.
-                    + "        return myClass.someMethod();\n"
-                    + "    }\n"
-                    + "}\n"
-        };
-        assertViolations(
-                sourceCodes,
-                v -> {
-                    assertEquals("someMethod", v.getSourceVertexName());
-                    assertEquals("MyClass", v.getSourceDefiningType());
                 });
     }
 }
--- sfge/src/test/java/com/salesforce/rules/unusedmethod/StaticMethodsTest.java
+++ sfge/src/test/java/com/salesforce/rules/unusedmethod/StaticMethodsTest.java
@@ -1,5 +1,7 @@
 package com.salesforce.rules.unusedmethod;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -35,8 +37,8 @@ public class StaticMethodsTest extends BaseUnusedMethodTest {
      *
      * @param testedVisibility - The visibility of the tested method. Either "public" or "private",
      *     since static methods can't be protected and global methods are ineligible.
-     * @param callerStatic - Indicates whether the method that calls the tested method should be
-     *     static or not.
+     * @param callerScope - The scope of the method that calls the tested method. Either public
+     *     static or public.
      * @param invocation - The manner in which the tested method is invoked. Either "method1()" or
      *     "MyClass.method1()". The {@code this} keyword doesn't work for static methods.
      */
@@ -45,23 +47,21 @@ public class StaticMethodsTest extends BaseUnusedMethodTest {
         // - private/public tested method (static methods can't be "protected")
         // - static/instance caller method
         // - implicit/explicit type mention.
-        "public, true, method1()",
-        "public, true, MyClass.method1()",
-        "public, false, method1()",
-        "public, false, MyClass.method1()",
-        "private, true, method1()",
-        "private, true, MyClass.method1()",
-        "private, false, method1()",
-        "private, false, MyClass.method1()",
+        "public, public static, method1()",
+        "public, public static, MyClass.method1()",
+        "public, public, method1()",
+        "public, public, MyClass.method1()",
+        "private, public static, method1()",
+        "private, public static, MyClass.method1()",
+        "private, public, method1()",
+        "private, public, MyClass.method1()",
     })
     @ParameterizedTest(name = "{displayName}: {0} static called by {1} as {2}")
     @Disabled
     public void staticInvokedByOwnClass_expectNoViolation(
-            String testedVisibility, boolean callerStatic, String invocation) {
+            String testedVisibility, String callerScope, String invocation) {
         // The tested method is always static.
         String testedScope = testedVisibility + " static";
-        // The calling method is either public static or just public.
-        String callerScope = callerStatic ? "public static" : "public";
         String sourceCode =
                 String.format(SIMPLE_USED_METHOD_SOURCE, testedScope, callerScope, invocation);
         assertNoViolations(sourceCode, 1);
@@ -112,8 +112,7 @@ public class StaticMethodsTest extends BaseUnusedMethodTest {
     /**
      * Tests for cases where a static method is called on a subclass of the class that defines it.
      *
-     * @param callersAreStatic - Indicates whether the method that calls the tested method should be
-     *     static
+     * @param callerScope - Scope of the subclass method that invokes the tested method.
      * @param callerClass - "child" or "grandchild" depending on which subclass should invoke the
      *     parent method
      * @param invocation - The manner in which the method is invoked. Can be implicit type reference
@@ -124,31 +123,28 @@ public class StaticMethodsTest extends BaseUnusedMethodTest {
         // - Static/instance caller method
         // - Calling in child vs calling in grandchild
         // - Calling via implicit type reference, or reference to specific class
-        "true, child, methodOnParent()",
-        "true, child, ParentClass.methodOnParent()",
-        "true, child, ChildClass.methodOnParent()",
-        "true, grandchild, methodOnParent()",
-        "true, grandchild, ParentClass.methodOnParent()",
-        "true, grandchild, ChildClass.methodOnParent()",
-        "true, grandchild, GrandchildClass.methodOnParent()",
-        "false, child, methodOnParent()",
-        "false, child, ParentClass.methodOnParent()",
-        "false, child, ChildClass.methodOnParent()",
-        "false, grandchild, methodOnParent()",
-        "false, grandchild, ParentClass.methodOnParent()",
-        "false, grandchild, ChildClass.methodOnParent()",
-        "false, grandchild, GrandchildClass.methodOnParent()",
+        "public static, child, methodOnParent()",
+        "public static, child, ParentClass.methodOnParent()",
+        "public static, child, ChildClass.methodOnParent()",
+        "public static, grandchild, methodOnParent()",
+        "public static, grandchild, ParentClass.methodOnParent()",
+        "public static, grandchild, ChildClass.methodOnParent()",
+        "public static, grandchild, GrandchildClass.methodOnParent()",
+        "public, child, methodOnParent()",
+        "public, child, ParentClass.methodOnParent()",
+        "public, child, ChildClass.methodOnParent()",
+        "public, grandchild, methodOnParent()",
+        "public, grandchild, ParentClass.methodOnParent()",
+        "public, grandchild, ChildClass.methodOnParent()",
+        "public, grandchild, GrandchildClass.methodOnParent()",
     })
     @ParameterizedTest(name = "{displayName}: Called by {0} in {1} as {2}")
     @Disabled
     public void staticInvokedBySubclass_expectNoViolation(
-            boolean callersAreStatic, String callerClass, String invocation) {
+            String callerScope, String callerClass, String invocation) {
         // The tested method is always public static. No need for private/protected, since private
         // is inaccessible in subclasses and static method can't be protected.
         String testedScope = "public static";
-        // The child and grandchild method are always public, but may also be static.
-        String childScope = callersAreStatic ? "public static" : "public";
-        String grandchildScope = callersAreStatic ? "public static" : "public";
         // Either the child or grandchild will invoke the parent method. The other will return a
         // literal true.
         String childReturn = callerClass.equalsIgnoreCase("child") ? invocation : "true";
@@ -158,9 +154,8 @@ public class StaticMethodsTest extends BaseUnusedMethodTest {
         String[] sourceCodes =
                 new String[] {
                     String.format(SUBCLASS_NON_OVERRIDDEN_SOURCES[0], testedScope),
-                    String.format(SUBCLASS_NON_OVERRIDDEN_SOURCES[1], childScope, childReturn),
-                    String.format(
-                            SUBCLASS_NON_OVERRIDDEN_SOURCES[2], grandchildScope, grandchildReturn)
+                    String.format(SUBCLASS_NON_OVERRIDDEN_SOURCES[1], callerScope, childReturn),
+                    String.format(SUBCLASS_NON_OVERRIDDEN_SOURCES[2], callerScope, grandchildReturn)
                 };
         assertNoViolations(sourceCodes, 1);
     }
@@ -180,7 +175,7 @@ public class StaticMethodsTest extends BaseUnusedMethodTest {
         // spotless:off
         String[] sourceCodes = new String[]{
             // Parent class
-            "global class ParentClass {\n"
+            "global virtual class ParentClass {\n"
             // Declare a static method to test. Must be public, since static can't be protected.
           + "    public static boolean testedMethod() {\n"
           + "        return true;\n"
@@ -196,6 +191,45 @@ public class StaticMethodsTest extends BaseUnusedMethodTest {
             // Invoke the tested method
           + "            " + invocation + ";\n"
           + "        }\n"
+          + "    }\n"
+          + "}"
+        };
+        // spotless:on
+        assertNoViolations(sourceCodes, 1);
+    }
+
+    /**
+     * Tests for cases where an inner class inherits a static method, and the static method is
+     * invoked by the outer class.
+     *
+     * @param invocation - The manner in which the method is called. Can reference the parent class,
+     *     or the child class (with/without full type reference).
+     */
+    @ValueSource(
+            strings = {
+                "ParentClass.testedMethod()",
+                "ChildClass.testedMethod()",
+                "OuterClass.ChildClass.testedMethod()"
+            })
+    @ParameterizedTest(name = "{displayName}: Invoked as {0}")
+    @Disabled
+    public void staticInnerInvokedByOuter_expectNoViolation(String invocation) {
+        // spotless:off
+        String[] sourceCodes = new String[]{
+            // Declare an extensible class with a static method. This is our tested method.
+            "global virtual class ParentClass {\n"
+          + "    public static boolean testedMethod() {\n"
+          + "        return true;\n"
+          + "    }\n"
+          + "}",
+            // Declare an outer class...
+            "global class OuterClass {\n"
+            // ...with an inner class that extends the tested class...
+          + "    global class ChildClass extends ParentClass {}\n"
+            // ...and a method that configurably calls the tested static method.
+          + "    /* sfge-disable-stack UnusedMethodRule */\n"
+          + "    public boolean invoker() {\n"
+          + "        return " + invocation + ";\n"
           + "    }\n"
           + "}"
         };
@@ -240,7 +274,127 @@ public class StaticMethodsTest extends BaseUnusedMethodTest {
             String methodVisibility, String propScope, String invocation) {
         String sourceCode =
                 String.format(
-                        METHOD_INVOKED_AS_PROPERTY_SOURCE, propScope, invocation, methodVisibility);
+                        METHOD_INVOKED_AS_PROPERTY_SOURCE,
+                        propScope,
+                        invocation,
+                        methodVisibility + " static");
         assertNoViolations(sourceCode, 1);
+    }
+
+    /**
+     * These tests cover variants of a weird edge case: If an inner class defines/inherits an
+     * instance method with the same name as a static method on its outer class, then it can invoke
+     * that instance method with the same syntax that would normally be an internal invocation of
+     * the outer static method (e.g., {@code method()}). In this case, the static method should
+     * count as unused.
+     *
+     * @param collidingMethod - The name to be used for the static method.
+     */
+    @ValueSource(strings = {"methodFromInnerClass", "methodFromParentClass", "methodFromInterface"})
+    @ParameterizedTest(name = "{displayName}: Collision with {0}")
+    @Disabled
+    public void internalReferenceSyntaxCollision_expectViolation(String collidingMethod) {
+        // spotless:off
+        String[] sourceCodes = new String[]{
+            // Declare an outer class with a static method. This will be our tested method.
+            "global class MyClass {\n"
+          + "    public static boolean " + collidingMethod + "() {\n"
+          + "        return true;\n"
+          + "    }\n"
+            // Declare an abstract inner class that has a method, and inherits methods from both an interface
+            // and another class.
+          + "    global abstract class InnerClass extends ParentClass implements ParentInterface {\n"
+            // The inner class has a method annotated to not trip the rule.
+          + "        /* sfge-disable-stack UnusedMethodRule */\n"
+          + "        public boolean methodFromInnerClass() {\n"
+          + "            return false;\n"
+          + "        }\n"
+            // The inner class also has a method that invokes the colliding method.
+          + "        /* sfge-disable-stack UnusedMethodRule */\n"
+          + "        public boolean callMethod() {\n"
+            // IMPORTANT! THE COLLIDING METHOD IS INVOKED WITHOUT ANY QUALIFIERS,
+          + "            return " + collidingMethod + "();\n"
+          + "        }\n"
+          + "    }\n"
+          + "}",
+            // Declare a parent class for the inner class to extend
+            "global virtual class ParentClass {\n"
+            // Give the parent class a method for the inner class to inherit.
+          + "    /* sfge-disable-stack UnusedMethodRule */\n"
+          + "    public boolean methodFromParentClass() {\n"
+          + "        return true;\n"
+          + "    }\n"
+          + "}",
+            // Declare an interface for the inner class to implement
+            "global interface ParentInterface {\n"
+          + "    boolean methodFromInterface();\n"
+          + "}"
+        };
+        // spotless:on
+        assertViolations(
+                sourceCodes,
+                v -> {
+                    assertEquals("MyClass", v.getSourceDefiningType());
+                    assertEquals(collidingMethod, v.getSourceVertexName());
+                });
+    }
+
+    /**
+     * These tests cover variants of a weird edge case: The syntax for external calls of static
+     * methods (i.e., {@code SomeClass.someMethod()}) is identical to the syntax for calling {@code
+     * someMethod()} on a variable/property/whatever named {@code SomeClass}. In this case, the
+     * static method should count as unused.
+     *
+     * @param collidingName - The name given to the tested class to cause a collision.
+     */
+    @ValueSource(strings = {"instanceProp", "staticProp", "methodParam", "variable", "InnerClass"})
+    @ParameterizedTest(name = "{displayName}: Collides with {0}")
+    @Disabled
+    public void externalReferenceSyntaxCollision_expectViolation(String collidingName) {
+        // spotless:off
+        String[] sourceCodes = new String[]{
+            // Declare a class with a static method and a configurable name. That's our  tested method.
+            "global class " + collidingName + " {\n"
+          + "    public static boolean getBoolean() {\n"
+          + "        return true;\n"
+          + "    }\n"
+          + "}",
+            // Declare a class with an instance method for the tested method to collide with.
+            "global class InstanceCollider {\n"
+          + "    /* sfge-disable-stack UnusedMethodRule */\n"
+          + "    public boolean getBoolean() {\n"
+          + "        return true;\n"
+          + "    }\n"
+          + "}",
+            // Declare a class with a static method for the tested method to collide with.
+            "global virtual class StaticCollider {\n"
+          + "    /* sfge-disable-stack UnusedMethodRule */\n"
+          + "    public static boolean getBoolean () {\n"
+          + "        return true;\n"
+          + "    }\n"
+          + "}",
+            // Declare a class that will do all sorts of collision-y operations.
+            "global class CollisionCreator {\n"
+            // These properties can be collided with.
+          + "    public InstanceCollider instanceProp;\n"
+          + "    public static InstanceCollider staticProp;\n"
+            // This inner class extends StaticCollider, meaning it has the static
+            // method and can therefore cause collisions.
+          + "    public class InnerClass extends StaticCollider {}\n"
+            // IMPORTANT: This method allows us to create a configurable collision.
+          + "    /* sfge-disable-stack UnusedMethodRule */\n"
+          + "    public boolean causeCollision(InstanceCollider methodParam) {\n"
+          + "        InstanceCollider variable = new InstanceCollider();\n"
+          + "        return " + collidingName + ".getBoolean();\n"
+          + "    }\n"
+          + "}"
+        };
+        // spotless:on
+        assertViolations(
+                sourceCodes,
+                v -> {
+                    assertEquals("getBoolean", v.getSourceVertexName());
+                    assertEquals(collidingName, v.getSourceDefiningType());
+                });
     }
 }
