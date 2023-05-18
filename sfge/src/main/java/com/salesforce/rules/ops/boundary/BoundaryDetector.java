e1b36dd6cfc56bf6a42d5eca3d01dec67568a56d
Roopa Mohan <rmohan@salesforce.com>
2023-05-18 10:20:07 -0700

    Additional documentation

--- sfge/src/main/java/com/salesforce/rules/ops/boundary/BoundaryDetector.java
+++ sfge/src/main/java/com/salesforce/rules/ops/boundary/BoundaryDetector.java
@@ -38,21 +38,30 @@ public abstract class BoundaryDetector<T extends Boundary<R>, R> {
         return Optional.of(this.boundaries.peek());
     }
 
+    /**
+     * Start extent of a new boundary
+     * @param boundary item that governs this extent.
+     */
     public void pushBoundary(T boundary) {
         LOGGER.debug("Entering loop boundary with boundaryItem=" + boundary.getBoundaryItem());
         push(boundary);
     }
 
+    /**
+     * End extent of a boundary. Performs additional checks to confirm validity.
+     * @param boundaryItem that is expected to govern the current boundary that's to be ended.
+     */
     public void popBoundary(R boundaryItem) {
         LOGGER.debug("Exiting boundary with boundaryItem=" + boundaryItem);
 
-        // Perform checks to ensure correctness of logic
+        // Check if a boundary is actually in place
         Optional<T> boundaryOptional = peek();
         if (!boundaryOptional.isPresent()) {
             throw new ProgrammingException(
                     "No boundary has been pushed to get a corresponding pop.");
         }
 
+        // Check if the existing boundary has the same boundary item as the end item
         R existingBoundaryItem = boundaryOptional.get().getBoundaryItem();
         if (!boundaryItem.equals(existingBoundaryItem)) {
             throw new UnexpectedException(
@@ -62,6 +71,7 @@ public abstract class BoundaryDetector<T extends Boundary<R>, R> {
                             + boundaryItem);
         }
 
+        // Remove the boundary
         pop();
     }
 }
