977abb703b10de96781dc382a748c53396e9e7aa
Joshua Feingold <jfeingold35@gmail.com>
2023-09-20 14:42:11 -0500

    CHANGE (GraphEngine): @W-14139399@: PerformNullcheckOnSoqlVariables now GA.

--- sfge/src/main/java/com/salesforce/rules/PerformNullCheckOnSoqlVariables.java
+++ sfge/src/main/java/com/salesforce/rules/PerformNullCheckOnSoqlVariables.java
@@ -62,6 +62,11 @@ public final class PerformNullCheckOnSoqlVariables extends AbstractPathTraversal
         return true;
     }
 
+    @Override
+    protected boolean isPilot() {
+        return false;
+    }
+
     /**
      * Tests a vertex using a symbol provider to check if it violates this rule.
      *
