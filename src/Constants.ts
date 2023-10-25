c9d590d2f917736024a81d1547a3741c4730b5ed
Joshua Feingold <jfeingold35@gmail.com>
2023-10-25 13:27:18 -0500

    CHANGE (CodeAnalyzer): @W-14329763@: Test config now set via env-var instead of IOC.

--- src/Constants.ts
+++ src/Constants.ts
@@ -3,21 +3,18 @@ import path = require('path');
 
 export const PMD_VERSION = '6.55.0';
 export const SFGE_VERSION = '1.0.1-pilot';
+export const DEFAULT_SCANNER_PATH = path.join(os.homedir(), 'sfdx-scanner');
 export const CATALOG_FILE = 'Catalog.json';
 export const CUSTOM_PATHS_FILE = 'CustomPaths.json';
 export const CONFIG_PILOT_FILE = 'Config-pilot.json';
 export const CONFIG_FILE = 'Config.json';
 export const PMD_CATALOG_FILE = 'PmdCatalog.json';
 
-export interface EnvOverridable {
-	getSfdxScannerPath(): string;
-}
-
-export class ProdOverrides implements EnvOverridable {
-	public getSfdxScannerPath(): string {
-		return path.join(os.homedir(), '.sfdx-scanner');
-	}
-}
+// TODO: We should flesh this one-off solution out into one that handles all the various env vars we use.
+//       E.g., the ones defined in `EnvironmentVariable.ts` and `dfa.ts`.
+export const ENV_VAR_NAMES = {
+	SCANNER_PATH_OVERRIDE: 'SCANNER_PATH_OVERRIDE'
+};
 
 export enum ENGINE {
 	PMD = 'pmd',
