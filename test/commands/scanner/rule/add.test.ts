2b27b5b3a934b8e6bdb421a2a94912eca17e6940
Joshua Feingold <jfeingold35@gmail.com>
2023-10-27 13:27:58 -0500

    @W-14329763@: Refactored scanner:rule:add tests.

--- test/commands/scanner/rule/add.test.ts
+++ test/commands/scanner/rule/add.test.ts
@@ -1,6 +1,7 @@
 import {expect} from '@salesforce/command/lib/test';
 import {Messages} from '@salesforce/core';
-import {setupCommandTest} from '../../../TestUtils';
+// @ts-ignore
+import {runCommand} from '../../../TestUtils';
 import * as os from 'os';
 import fs = require('fs');
 import path = require('path');
@@ -9,7 +10,7 @@ import path = require('path');
 Messages.importMessagesDirectory(__dirname);
 const messages = Messages.loadMessages('@salesforce/sfdx-scanner', 'add');
 
-describe('scanner:rule:add', () => {
+describe('scanner rule add', () => {
 	describe('E2E', () => {
 		const myLanguage = 'apex';
 		describe('Test Case: Adding individual JARs', () => {
@@ -27,54 +28,43 @@ describe('scanner:rule:add', () => {
 			const tmpJar4 = path.join(tmpDir4, 'bar4.jar');
 			fs.writeFileSync(tmpJar4, 'Heat and cold, life and death, and of course, light and dark.');
 
-			// For the first test, we'll add two of the JARs with their absolute paths.
-			const absolutePaths = [tmpJar1, tmpJar2];
-			setupCommandTest
-				.command(['scanner:rule:add',
-					'--language', myLanguage,
-					'--path', absolutePaths.join(','),
-					'--json'
-				])
-				.it('Individual JARs can be added with their absolute path', ctx => {
-					// The expectation is that the paths will just be used as-is.
-					const expectedPaths = absolutePaths;
-					const outputJson = JSON.parse(ctx.stdout);
-					const result = outputJson.result;
-					expect(result).to.have.property('success')
-						.and.equals(true);
+			it('Individual JARs can be added with their absolute path', () => {
+				// For the first test, we'll add two of the JARs with their absolute paths.
+				const absolutePaths = [tmpJar1, tmpJar2];
+				const output = runCommand(`scanner rule add --language ${myLanguage} --path ${absolutePaths.join(',')} --json`);
+				// The expectation is that the paths will just be used as-is.
+				const expectedPaths = absolutePaths;
+				const outputJson = output.jsonOutput;
+				const result = outputJson.result;
+				expect(result).to.have.property('success')
+					.and.equals(true);
 
-					expect(result).to.have.property('language')
-						.and.equals(myLanguage);
+				expect(result).to.have.property('language')
+					.and.equals(myLanguage);
 
-					expect(result).to.have.property('path')
-						.and.have.lengthOf(expectedPaths.length)
-						.and.deep.equals(expectedPaths);
-				});
+				expect(result).to.have.property('path')
+					.and.have.lengthOf(expectedPaths.length)
+					.and.deep.equals(expectedPaths);
+			});
 
+			it('Individual JARs can be added with their relative paths', () => {
+				// For the second test, we'll add the other two JARs with relative paths.
+				const relativePaths = [path.relative('.', tmpJar3), path.relative('.', tmpJar4)];
+				const output = runCommand(`scanner rule add --language ${myLanguage} --path ${relativePaths.join(',')} --json`);
+				// The expectation is that the paths will be converted to absolute paths.
+				const expectedPaths = [tmpJar3, tmpJar4];
+				const outputJson = output.jsonOutput;
+				const result = outputJson.result;
+				expect(result).to.have.property('success')
+					.and.equals(true);
 
-			// For the second test, we'll add the other two JARs with relative paths.
-			const relativePaths = [path.relative('.', tmpJar3), path.relative('.', tmpJar4)];
-			setupCommandTest
-				.command(['scanner:rule:add',
-					'--language', myLanguage,
-					'--path', relativePaths.join(','),
-					'--json'
-				])
-				.it('Individual JARs can be added with their relative paths', ctx => {
-					// The expectation is that the paths will be converted to absolute paths.
-					const expectedPaths = [tmpJar3, tmpJar4];
-					const outputJson = JSON.parse(ctx.stdout);
-					const result = outputJson.result;
-					expect(result).to.have.property('success')
-						.and.equals(true);
+				expect(result).to.have.property('language')
+					.and.equals(myLanguage);
 
-					expect(result).to.have.property('language')
-						.and.equals(myLanguage);
-
-					expect(result).to.have.property('path')
-						.and.have.lengthOf(expectedPaths.length)
-						.and.deep.equals(expectedPaths);
-				});
+				expect(result).to.have.property('path')
+					.and.have.lengthOf(expectedPaths.length)
+					.and.deep.equals(expectedPaths);
+			});
 		});
 
 
@@ -96,84 +86,68 @@ describe('scanner:rule:add', () => {
 			fs.writeFileSync(tmpJar6, 'With the strength of Lords, they challenged the Dragons.');
 
 			// For the first test, we'll add one of the folders by its absolute path.
-			setupCommandTest
-				.command(['scanner:rule:add',
-					'--language', myLanguage,
-					'--path', tmpDir1,
-					'--json'
-				])
-				.it('Folders can be added with their absolute paths', ctx => {
-					// The expectation is that all three JARs in the folder will have been added.
-					const expectedPaths = [tmpJar1, tmpJar2, tmpJar3];
-					const outputJson = JSON.parse(ctx.stdout);
-					const result = outputJson.result;
-					expect(result).to.have.property('success')
-						.and.equals(true);
+			it('Folders can be added with their absolute paths', () => {
+				const output = runCommand(`scanner rule add --language ${myLanguage} --path ${tmpDir1} --json`);
+				// The expectation is that all three JARs in the folder will have been added.
+				const expectedPaths = [tmpJar1, tmpJar2, tmpJar3];
+				const result = output.jsonOutput.result;
+				expect(result).to.have.property('success')
+					.and.equals(true);
 
-					expect(result).to.have.property('language')
-						.and.equals(myLanguage);
+				expect(result).to.have.property('language')
+					.and.equals(myLanguage);
 
-					expect(result).to.have.property('path')
-						.and.have.lengthOf(expectedPaths.length)
-						.and.deep.equals(expectedPaths);
-				});
+				expect(result).to.have.property('path')
+					.and.have.lengthOf(expectedPaths.length)
+					.and.deep.equals(expectedPaths);
+			});
 
 			// For the second test, we'll add the other folder by its relative path.
-			setupCommandTest
-				.command(['scanner:rule:add',
-					'--language', myLanguage,
-					'--path', path.relative('.', tmpDir2),
-					'--json'
-				])
-				.it('Folders can be added by their relative paths', ctx => {
-					// The expectation is that all three JARs in the folder will have been added.
-					const expectedPaths = [tmpJar4, tmpJar5, tmpJar6];
-					const outputJson = JSON.parse(ctx.stdout);
-					const result = outputJson.result;
-					expect(result).to.have.property('success')
-						.and.equals(true);
+			it('Folders can be added by their relative paths', () => {
+				const output = runCommand(`scanner rule add --language ${myLanguage} --path ${path.relative('.', tmpDir2)} --json`);
+				// The expectation is that all three JARs in the folder will have been added.
+				const expectedPaths = [tmpJar4, tmpJar5, tmpJar6];
+				const result = output.jsonOutput.result;
+				expect(result).to.have.property('success')
+					.and.equals(true);
 
-					expect(result).to.have.property('language')
-						.and.equals(myLanguage);
+				expect(result).to.have.property('language')
+					.and.equals(myLanguage);
 
-					expect(result).to.have.property('path')
-						.and.have.lengthOf(expectedPaths.length)
-						.and.deep.equals(expectedPaths);
-				});
+				expect(result).to.have.property('path')
+					.and.have.lengthOf(expectedPaths.length)
+					.and.deep.equals(expectedPaths);
+			});
 		});
 	});
 
 	describe('Validations', () => {
 		describe('Language validations', () => {
 			// Test for failure scenario doesn't need to do any special setup or cleanup.
-			setupCommandTest
-				.command(['scanner:rule:add', '--path', '/some/local/path'])
-				.it('should complain about missing --language flag', ctx => {
-					expect(ctx.stderr).contains('Missing required flag language');
-				});
+			it('should complain about missing --language flag', () => {
+				const output = runCommand(`scanner rule add --path /some/local/path`);
+				expect(output.shellOutput.stderr).to.contain('Missing required flag language');
+			});
 
 			// Test for failure scenario doesn't need to do any special setup or cleanup.
-			setupCommandTest
-				.command(['scanner:rule:add', '--language', '', '--path', '/some/local/path'])
-				.it('should complain about empty language entry', ctx => {
-					expect(ctx.stderr).contains(messages.getMessage('validations.languageCannotBeEmpty'));
-				});
+			it('should complain about empty language entry', () => {
+				const output = runCommand(`scanner rule add --language '' --path /some/local/path`);
+				expect(output.shellOutput.stderr).to.contain(messages.getMessage('validations.languageCannotBeEmpty'));
+			});
 		});
 
 		describe('Path validations', () => {
 			// Test for failure scenario doesn't need to do any special setup or cleanup.
-			setupCommandTest
-				.command(['scanner:rule:add', '--language', 'apex'])
-				.it('should complain about missing --path flag', ctx => {
-					expect(ctx.stderr).contains('Missing required flag path');
-				});
+			it('should complain about missing --path flag', () => {
+				const output = runCommand(`scanner rule add --language apex`);
+				expect(output.shellOutput.stderr).to.contain('Missing required flag path');
+			});
 
 			// Test for failure scenario doesn't need to do any special setup or cleanup.
-			setupCommandTest
-				.command(['scanner:rule:add', '--language', 'apex', '--path', ''])
-				.it('should complain about empty path', ctx => {
-					expect(ctx.stderr).contains(messages.getMessage('validations.pathCannotBeEmpty'));
-				});
+			it('should complain about empty path', () => {
+				const output = runCommand(`scanner rule add --language apex --path ''`);
+				expect(output.shellOutput.stderr).to.contain(messages.getMessage('validations.pathCannotBeEmpty'));
+			});
 		});
 	});
 });
