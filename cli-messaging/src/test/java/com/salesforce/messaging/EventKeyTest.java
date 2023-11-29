5886ed78d4fff4a1b8dbcfb526af0af83d2abb60
Stephen Carter <stephen.carter@salesforce.com>
2023-11-29 16:30:46 -0500

    @W-14573108@ Tabs to spaces fix

--- cli-messaging/src/test/java/com/salesforce/messaging/EventKeyTest.java
+++ cli-messaging/src/test/java/com/salesforce/messaging/EventKeyTest.java
@@ -27,76 +27,76 @@ import java.util.stream.Stream;
 public class EventKeyTest {
     private static final String INFO_PREFIX = "info";
     private static final String WARNING_PREFIX = "warning";
-	private static final String ERROR_EXTERNAL_PREFIX = "error.external";
-	private static final String ERROR_INTERNAL_PREFIX = "error.internal";
+    private static final String ERROR_EXTERNAL_PREFIX = "error.external";
+    private static final String ERROR_INTERNAL_PREFIX = "error.internal";
 
-	// Current path is sfdx-scanner/pmd-cataloger
-	private static final String MESSAGES_FILE = "../messages/EventKeyTemplates.js";
+    // Current path is sfdx-scanner/pmd-cataloger
+    private static final String MESSAGES_FILE = "../messages/EventKeyTemplates.js";
 
-	JSONObject jsonObject = null;
+    JSONObject jsonObject = null;
 
-	@BeforeEach
-	public void extractMessagesJson() throws IOException, ParseException {
-		final Path path = Paths.get(MESSAGES_FILE);
-		assertThat("Invalid test setup. File does not exist: " + MESSAGES_FILE, Files.exists(path), is(true));
-		final String fileContent = new String(Files.readAllBytes(path));
-		final String[] fileSplit = fileContent.split("=");
-		final int fileParts = fileSplit.length;
-		assertThat("Invalid test setup. File has more than one '=', which caused confusion in picking JSON content. Please revisit messages in " + MESSAGES_FILE, fileParts, is(2));
-		final String jsonContent = fileSplit[1];
-		jsonObject = (JSONObject) new JSONParser().parse(jsonContent);
-		assertThat("Invalid test setup. Messages json has not been parsed correctly. Please check validity of " + MESSAGES_FILE, jsonObject, is(notNullValue()));
-	}
+    @BeforeEach
+    public void extractMessagesJson() throws IOException, ParseException {
+        final Path path = Paths.get(MESSAGES_FILE);
+        assertThat("Invalid test setup. File does not exist: " + MESSAGES_FILE, Files.exists(path), is(true));
+        final String fileContent = new String(Files.readAllBytes(path));
+        final String[] fileSplit = fileContent.split("=");
+        final int fileParts = fileSplit.length;
+        assertThat("Invalid test setup. File has more than one '=', which caused confusion in picking JSON content. Please revisit messages in " + MESSAGES_FILE, fileParts, is(2));
+        final String jsonContent = fileSplit[1];
+        jsonObject = (JSONObject) new JSONParser().parse(jsonContent);
+        assertThat("Invalid test setup. Messages json has not been parsed correctly. Please check validity of " + MESSAGES_FILE, jsonObject, is(notNullValue()));
+    }
 
     @ParameterizedTest(name = "eventKey={0}")
     @MethodSource("getAllEventKeyValues")
-	public void verifyKeyInJson(EventKey eventKey) {
-		// Split messageKey into levels
-		final String messageKey = eventKey.getMessageKey();
-		final String[] levels = messageKey.split("\\.");
+    public void verifyKeyInJson(EventKey eventKey) {
+        // Split messageKey into levels
+        final String messageKey = eventKey.getMessageKey();
+        final String[] levels = messageKey.split("\\.");
 
-		// Loop through JSON to verify presence of each level
-		int idx = 0;
-		JSONObject currentJsonContent = this.jsonObject;
-		while (idx < levels.length - 1) {
-			currentJsonContent = (JSONObject) currentJsonContent.get(levels[idx]);
-			assertThat("Level " + levels[idx] + " not found. Recheck value of messageKey " + messageKey + " in EventKey." + eventKey, currentJsonContent, is(notNullValue()));
-			idx++;
-		}
-		final Object lastLevel = currentJsonContent.get(levels[levels.length - 1]);
-		assertThat("messageKey " + messageKey + " does not exist. Recheck EventKey." + eventKey, lastLevel, is(notNullValue()));
-		assertThat("Message value should be a String for messageKey " + messageKey + " in EventKey." + eventKey, lastLevel instanceof String, is(true));
-	}
+        // Loop through JSON to verify presence of each level
+        int idx = 0;
+        JSONObject currentJsonContent = this.jsonObject;
+        while (idx < levels.length - 1) {
+            currentJsonContent = (JSONObject) currentJsonContent.get(levels[idx]);
+            assertThat("Level " + levels[idx] + " not found. Recheck value of messageKey " + messageKey + " in EventKey." + eventKey, currentJsonContent, is(notNullValue()));
+            idx++;
+        }
+        final Object lastLevel = currentJsonContent.get(levels[levels.length - 1]);
+        assertThat("messageKey " + messageKey + " does not exist. Recheck EventKey." + eventKey, lastLevel, is(notNullValue()));
+        assertThat("Message value should be a String for messageKey " + messageKey + " in EventKey." + eventKey, lastLevel instanceof String, is(true));
+    }
 
     @ParameterizedTest(name = "eventKey={0}")
     @MethodSource("getAllInfoEventKeyValues")
-	public void verifyInfo(EventKey eventKey) {
-		assertThat("Unexpected messageType on EventKey." + eventKey, eventKey.getMessageType(), is(MessageType.INFO));
+    public void verifyInfo(EventKey eventKey) {
+        assertThat("Unexpected messageType on EventKey." + eventKey, eventKey.getMessageType(), is(MessageType.INFO));
         // No verbose check since we don't have a specific rule for verbosity on info
-	}
+    }
 
     @ParameterizedTest(name = "eventKey={0}")
     @MethodSource("getAllWarningEventKeyValues")
-	public void verifyWarning(EventKey eventKey) {
-		assertThat("Unexpected messageType on EventKey." + eventKey, eventKey.getMessageType(), is(MessageType.WARNING));
-		// No verbose check since we don't have a specific rule for verbosity on warning
-	}
+    public void verifyWarning(EventKey eventKey) {
+        assertThat("Unexpected messageType on EventKey." + eventKey, eventKey.getMessageType(), is(MessageType.WARNING));
+        // No verbose check since we don't have a specific rule for verbosity on warning
+    }
 
     @ParameterizedTest(name = "eventKey={0}")
     @MethodSource("getAllErrorExternalEventKeyValues")
-	public void verifyErrorExternal(EventKey eventKey) {
-		assertThat("Unexpected messageType on EventKey." + eventKey, eventKey.getMessageType(), is(MessageType.ERROR));
-		assertThat("Verbose value on external error messages should be False. Please recheck EventKey." + eventKey, eventKey.isVerbose(), is(false));
-		assertThat("MessageHandler on external error messages should be UX. Please recheck EventKey." + eventKey, eventKey.getMessageHandler(), is(MessageHandler.UX));
-	}
+    public void verifyErrorExternal(EventKey eventKey) {
+        assertThat("Unexpected messageType on EventKey." + eventKey, eventKey.getMessageType(), is(MessageType.ERROR));
+        assertThat("Verbose value on external error messages should be False. Please recheck EventKey." + eventKey, eventKey.isVerbose(), is(false));
+        assertThat("MessageHandler on external error messages should be UX. Please recheck EventKey." + eventKey, eventKey.getMessageHandler(), is(MessageHandler.UX));
+    }
 
     @ParameterizedTest(name = "eventKey={0}")
     @MethodSource("getAllErrorInternalEventKeyValues")
-	public void verifyErrorInternal(EventKey eventKey) {
-		assertThat("Unexpected messageType on EventKey." + eventKey, eventKey.getMessageType(), is(MessageType.ERROR));
-		assertThat("Verbose value on internal error messages should be False. Please recheck EventKey." + eventKey, eventKey.isVerbose(), is(false));
-		assertThat("MessageHandler on internal error messages should be INTERNAL. Please recheck EventKey." + eventKey, eventKey.getMessageHandler(), is(MessageHandler.INTERNAL));
-	}
+    public void verifyErrorInternal(EventKey eventKey) {
+        assertThat("Unexpected messageType on EventKey." + eventKey, eventKey.getMessageType(), is(MessageType.ERROR));
+        assertThat("Verbose value on internal error messages should be False. Please recheck EventKey." + eventKey, eventKey.isVerbose(), is(false));
+        assertThat("MessageHandler on internal error messages should be INTERNAL. Please recheck EventKey." + eventKey, eventKey.getMessageHandler(), is(MessageHandler.INTERNAL));
+    }
 
     public static Stream<EventKey> getAllEventKeyValues() {
         return Arrays.stream(EventKey.values());
