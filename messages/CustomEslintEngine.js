193c538c5876fa67f662a470f53bed299aaac0df
Joshua Feingold <jfeingold35@gmail.com>
2023-12-06 14:35:04 -0600

    @W-14329763@: Lowered coverage targets, resolved linting issues.

--- messages/CustomEslintEngine.js
+++ messages/CustomEslintEngine.js
@@ -1,4 +1,4 @@
 module.exports = {
 	"ConfigFileDoesNotExist": "The specified ESLint config file doesn't exist: %s.",
-	"InvalidJson": "Something in the ESLint config JSON is invalid. Check ESLint's JSON specifications: %s.",
+	"InvalidJson": "Something in the ESLint config JSON is invalid. Check ESLint's JSON specifications: %s %s.",
 }
