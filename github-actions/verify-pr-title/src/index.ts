905ca04432a3e258d2cf539ea4d6829a788d29af
Josh Feingold <jfeingold@salesforce.com>
2024-04-17 20:20:57 -0500

    CHANGE (GithubActions) @W-15362391@ Modified Github Actions for flexibility. (#1441)

--- github-actions/verify-pr-title/src/index.ts
+++ github-actions/verify-pr-title/src/index.ts
@@ -1,6 +1,8 @@
 import core = require("@actions/core");
 import github = require("@actions/github");
-import { verifyDevBranchPrTitle, verifyReleaseBranchPrTitle } from "./verifyPrTitle";
+import {verifyReleasePrTitle} from "./verifyReleasePrTitle";
+import {verifyMain2DevPrTitle} from "./verifyMain2DevPrTitle";
+import {verifyFeaturePrTitle} from "./verifyFeaturePrTitle";
 
 /**
  * Verifies that a pull request title conforms to the pattern required by git2gus
@@ -19,30 +21,43 @@ function run(): void {
 		// Examine the title for the expected patterns, which vary depending on what the target branch is.
 		const title = pullRequest.title as string;
 		const base = pullRequest.base as {ref: string};
-		const targetBranch = base.ref;
-		if (targetBranch === "release" || targetBranch === "documentation") {
-			// Release branches have their own less stringent format.
-			if (verifyReleaseBranchPrTitle(title)) {
-				console.log(`PR title '${title}' accepted for release branch.`);
-			} else {
-				core.setFailed(
-					`PR title '${title}' does not match the release PR template of "RELEASE: @W-XXXX@: Summary".`
-				);
-				return;
-			}
-		} else if (targetBranch === "dev" || targetBranch === "docdev") {
-			// Dev branches have a more stringent format.
-			if (verifyDevBranchPrTitle(title)) {
+		const baseBranch = base.ref;
+		const head = pullRequest.head as {ref: string};
+		const headBranch = head.ref;
+		if (headBranch === "main" && baseBranch === "dev") {
+			// There's a title convention for merging `main` into `dev`.
+			if (verifyMain2DevPrTitle(title)) {
 				console.log(`PR title '${title}' accepted for dev branch.`);
 			} else {
 				core.setFailed(
-					`PR title '${title}' does not match the dev PR template of "TYPE (SCOPE): @W-XXXX@: Summary"`
+					`PR title '${title}' does not match the template of "Main2Dev @W-XXXX@ Rebasing after vX.Y.Z"`
+				);
+				return;
+			}
+		} else if (baseBranch === "release" || baseBranch === "main") {
+			// There's a title convention for merging PRs into `release`/`main`.
+			if (verifyReleasePrTitle(title)) {
+				console.log(`PR title '${title}' accepted for ${baseBranch} branch`);
+			} else {
+				core.setFailed(
+					`PR title '${title}' does not match the template of "RELEASE @W-XXXX@ Summary"`
+				);
+				return;
+			}
+		} else if (baseBranch == "dev" || /^release-\d+\.\d+\.\d+$/.test(baseBranch)) {
+			// There's a title convention for merging feature branch PRs into `dev` or `release-X.Y.Z`
+			// branches.
+			if (verifyFeaturePrTitle(title)) {
+				console.log(`PR title '${title}' accepted for ${baseBranch} branch`);
+			} else {
+				core.setFailed(
+					`PR title '${title}' does not match the template of "NEW|FIX|CHANGE (__) @W-XXXX@ Summary"`
 				);
 				return;
 			}
 		} else {
-			// Not sure why you'd have a pull request aimed at some other branch, but that should probably be allowed.
-			console.log(`PR title '${title}' automatically accepted for non-release, non-dev branch`);
+			// For PRs aimed at any other branch, anything goes.
+			console.log(`PR title '${title}' automatically accepted for ${baseBranch} branch`);
 		}
 	} catch (error) {
 		if (error instanceof Error) {
