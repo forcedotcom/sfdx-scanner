3f3d6874a4db92d78f28b041d2fd62a9aa3b78fb
Joshua Feingold <jfeingold35@gmail.com>
2024-05-31 10:43:53 -0500

    Force fail

--- test/sample.test.ts
+++ test/sample.test.ts
@@ -3,7 +3,7 @@
 describe('Sample category', () => {
 	describe('Sample subcategory', () => {
 		it('Sample test', () => {
-			expect(2 + 2).toEqual(4);
+			expect(2 + 2).toEqual(5);
 		});
 	});
 });
