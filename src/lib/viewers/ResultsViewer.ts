d372730ee28685dc93f03d0f2ec8b1806d74fd5a
Joshua Feingold <jfeingold35@gmail.com>
2024-06-18 10:07:52 -0500

    NEW (ActionLayer) @W-15903094@ Miscellaneous refactoring.

--- src/lib/viewers/ResultsViewer.ts
+++ src/lib/viewers/ResultsViewer.ts
@@ -17,52 +17,22 @@ abstract class AbstractResultsViewer implements ResultsViewer {
 }
 
 export class ResultsDetailViewer extends AbstractResultsViewer {
+	/**
+	 * The display format consumes a lot of vertical space. As such, we're choosing to limit the total
+	 * number of violations that it will display before truncating the results and instructing the user
+	 * to consult one of the output files.
+	 */
 	public static readonly RESULTS_CUTOFF = 10;
 
 	public view(results: RunResults): void {
 		if (results.getViolationCount() === 0) {
 			this.display.displayInfo(getMessage(BundleName.ResultsViewer, 'summary.found-no-results'));
 		} else {
-			const violations = results.getViolations();
-			const violationCount = violations.length;
-			const fileCount = this.countUniqueFiles(violations);
-			// Output the total number of violations and files.
-			this.display.displayInfo(getMessage(BundleName.ResultsViewer, 'summary.found-results', [violationCount, fileCount]));
+			const violations = this.sortViolations(results.getViolations());
 
-			// Sort and output the violations.
-			const sortedViolations = this.sortViolations(violations);
-			const printableResultsCount = Math.min(ResultsDetailViewer.RESULTS_CUTOFF, sortedViolations.length);
-			const omittedResultsCount = sortedViolations.length - printableResultsCount;
-			for (let i = 0; i < printableResultsCount; i++) {
-				const violation = sortedViolations[i];
-				this.display.displayStyledHeader(getMessage(BundleName.ResultsViewer, 'summary.detail.violation-header', [i + 1, violation.getRule().getName()]));
-				const sev = violation.getRule().getSeverityLevel();
-				const primaryLocation = violation.getCodeLocations()[violation.getPrimaryLocationIndex()];
-				this.display.displayStyledObject({
-					engine: violation.getRule().getEngineName(),
-					severity: `${sev.valueOf()} (${SeverityLevel[sev]})`,
-					message: violation.getMessage(),
-					location: `${primaryLocation.getFile()}:${primaryLocation.getStartLine()}:${primaryLocation.getStartColumn()}`,
-					docs: violation.getResourceUrls().join(',')
-				}, ['engine', 'severity', 'message', 'location', 'docs']);
-			}
-
-			if (omittedResultsCount > 0) {
-				this.display.displayInfo(getMessage(BundleName.ResultsViewer, 'summary.detail.results-truncated', [omittedResultsCount]));
-			}
-
-			// Output the breakdown.
-			this.display.displayStyledHeader(getMessage(BundleName.ResultsViewer, 'summary.detail.breakdown.header'));
-			this.display.displayInfo(getMessage(BundleName.ResultsViewer, 'summary.detail.breakdown.total', [violations.length]));
-			for (const sev of Object.values(SeverityLevel)) {
-				if (typeof sev !== 'string') {
-					continue;
-				}
-				const sevCount = results.getViolationCountOfSeverity(SeverityLevel[sev] as SeverityLevel);
-				if (sevCount > 0) {
-					this.display.displayInfo(getMessage(BundleName.ResultsViewer, 'summary.detail.breakdown.item', [sevCount, sev]));
-				}
-			}
+			this.displaySummary(violations);
+			this.displayDetails(violations);
+			this.displayBreakdown(results);
 		}
 	}
 
@@ -107,6 +77,55 @@ export class ResultsDetailViewer extends AbstractResultsViewer {
 			const v2StartColumn = v2PrimaryLocation.getStartColumn() || 0;
 			return v1StartColumn - v2StartColumn;
 		});
+	}
+
+	private displaySummary(violations: Violation[]): void {
+		const violationCount = violations.length;
+		const fileCount = this.countUniqueFiles(violations);
+		this.display.displayInfo(getMessage(BundleName.ResultsViewer, 'summary.found-results', [violationCount, fileCount]));
+	}
+
+	private displayDetails(violations: Violation[]): void {
+		// If there are more results than the cutoff amount, we'll only show that many.
+		const printableViolationCount = Math.min(ResultsDetailViewer.RESULTS_CUTOFF, violations.length);
+		const omittedResultsCount = violations.length - printableViolationCount;
+		for (let i = 0; i < printableViolationCount; i++) {
+			const violation = violations[i];
+			const rule = violation.getRule();
+			const sev = rule.getSeverityLevel();
+			this.display.displayStyledHeader(
+				getMessage(
+					BundleName.ResultsViewer,
+					'summary.detail.violation-header',
+					[i + 1, rule.getName()])
+			);
+			const primaryLocation = violation.getCodeLocations()[violation.getPrimaryLocationIndex()];
+			this.display.displayStyledObject({
+				engine: rule.getEngineName(),
+				severity: `${sev.valueOf()} (${SeverityLevel[sev]})`,
+				message: violation.getMessage(),
+				location: `${primaryLocation.getFile()}:${primaryLocation.getStartLine()}:${primaryLocation.getStartColumn()}`,
+				docs: violation.getResourceUrls().join(',')
+			}, ['engine', 'severity', 'message', 'location', 'docs']);
+		}
+		if (omittedResultsCount > 0) {
+			this.display.displayInfo(getMessage(BundleName.ResultsViewer, 'summary.detail.results-truncated', [omittedResultsCount]));
+		}
+	}
+
+	private displayBreakdown(results: RunResults): void {
+		this.display.displayStyledHeader(getMessage(BundleName.ResultsViewer, 'summary.detail.breakdown.header'));
+		this.display.displayInfo(getMessage(BundleName.ResultsViewer, 'summary.detail.breakdown.total', [results.getViolationCount()]));
+		for (const sev of Object.values(SeverityLevel)) {
+			// Some of the keys will be numbers, since the enum is numerical. Skip those.
+			if (typeof sev !== 'string') {
+				continue;
+			}
+			const sevCount = results.getViolationCountOfSeverity(SeverityLevel[sev] as SeverityLevel);
+			if (sevCount > 0) {
+				this.display.displayInfo(getMessage(BundleName.ResultsViewer, 'summary.detail.breakdown.item', [sevCount, sev]));
+			}
+		}
 	}
 }
 
