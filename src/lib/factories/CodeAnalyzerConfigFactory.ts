7ca01d78e7cb1ce419294c66a63d1825a07eb740
Stephen Carter <stephen.carter@salesforce.com>
2024-06-20 09:57:11 -0400

    NEW (Internal): Wire up dynamic loading of custom_engine_plugin_modules

--- src/lib/factories/CodeAnalyzerConfigFactory.ts
+++ src/lib/factories/CodeAnalyzerConfigFactory.ts
@@ -8,6 +8,10 @@ export class CodeAnalyzerConfigFactoryImpl implements CodeAnalyzerConfigFactory 
 	public create(configPath?: string): CodeAnalyzerConfig {
 		if (configPath) {
 			return CodeAnalyzerConfig.fromFile(configPath);
+
+		// TODO: If the user did not provide a config-file then we should then check to see if the default one exists
+		// at path.join(process.cwd(),'code-analyzer-config.yml') and if so then use that instead of using withDefaults.
+		// This is important so that users don't always have to do --config-file <...> all the time.
 		} else {
 			return CodeAnalyzerConfig.withDefaults();
 		}
