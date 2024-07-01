423e2d7e007623b5783c7fc4d5516856fa0fdf8e
Joshua Feingold <jfeingold35@gmail.com>
2024-07-01 16:30:45 -0500

    NEW @W-15903177@ Table-formatted rule command output.

--- messages/rule-viewer.md
+++ messages/rule-viewer.md
@@ -10,3 +10,23 @@
 
 *DRAFT*: %d. %s
 
+# summary.table.id-column
+
+Id
+
+# summary.table.name-column
+
+Name
+
+# summary.table.engine-column
+
+Engine
+
+# summary.table.severity-column
+
+Severity
+
+
+# summary.table.tag-column
+
+Tag
--- test/lib/viewers/RuleViewer.test.ts
+++ test/lib/viewers/RuleViewer.test.ts
@@ -1,4 +1,4 @@
-import {RuleDetailViewer} from '../../../src/lib/viewers/RuleViewer';
+import {RuleDetailViewer, RuleTableViewer} from '../../../src/lib/viewers/RuleViewer';
 import {DisplayEventType, SpyDisplay} from '../../stubs/SpyDisplay';
 import * as StubRules from '../../stubs/StubRules';
 
@@ -105,15 +105,83 @@ describe('RuleViewer implementations', () => {
 
 	describe('RuleTableViewer', () => {
 		it('When given no rules, outputs summary and nothing else', () => {
+			const display = new SpyDisplay();
+			const viewer = new RuleTableViewer(display);
 
+			viewer.view([]);
+
+			const displayEvents = display.getDisplayEvents();
+			expect(displayEvents).toHaveLength(1);
+			expect(displayEvents).toEqual([{
+				type: DisplayEventType.LOG,
+				data: '*DRAFT*: Found 0 rules.'
+			}]);
 		});
 
 		it('When given one rule, outputs correct summary and rule data', () => {
+			const display = new SpyDisplay();
+			const viewer = new RuleTableViewer(display);
+			const rule = new StubRules.StubRule1();
 
+			viewer.view([
+				rule
+			]);
+
+			const displayEvents = display.getDisplayEvents();
+			expect(displayEvents).toHaveLength(2);
+			expect(displayEvents).toEqual([{
+				type: DisplayEventType.LOG,
+				data: '*DRAFT*: Found 1 rules:'
+			}, {
+				type: DisplayEventType.TABLE,
+				data: JSON.stringify({
+					columns: ['Id', 'Name', 'Engine', 'Severity', 'Tag'],
+					rows: [{
+						id: 1,
+						name: rule.getName(),
+						engine: rule.getEngineName(),
+						severity: rule.getFormattedSeverity(),
+						tag: rule.getFormattedTags()
+					}]
+				})
+			}])
 		});
 
 		it('When given multiple rules, outputs correct summary and rule data', () => {
+			const display = new SpyDisplay();
+			const viewer = new RuleTableViewer(display);
+			const rule1 = new StubRules.StubRule1();
+			const rule2 = new StubRules.StubRule2();
 
+			viewer.view([
+				rule1,
+				rule2
+			]);
+
+			const displayEvents = display.getDisplayEvents();
+			expect(displayEvents).toHaveLength(2);
+			expect(displayEvents).toEqual([{
+				type: DisplayEventType.LOG,
+				data: '*DRAFT*: Found 2 rules:'
+			}, {
+				type: DisplayEventType.TABLE,
+				data: JSON.stringify({
+					columns: ['Id', 'Name', 'Engine', 'Severity', 'Tag'],
+					rows: [{
+						id: 1,
+						name: rule1.getName(),
+						engine: rule1.getEngineName(),
+						severity: rule1.getFormattedSeverity(),
+						tag: rule1.getFormattedTags()
+					}, {
+						id: 2,
+						name: rule2.getName(),
+						engine: rule2.getEngineName(),
+						severity: rule2.getFormattedSeverity(),
+						tag: rule2.getFormattedTags()
+					}]
+				})
+			}]);
 		});
 	});
 });
