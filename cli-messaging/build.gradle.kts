8a8e9a6bf0142e4ecc59daf703438865ed548aac
Josh Feingold <jfeingold@salesforce.com>
2024-07-17 09:53:08 -0500

    CHANGE (CodeAnalyzer): @W-16206934@: Upgraded dependencies. (#1545)

--- cli-messaging/build.gradle.kts
+++ cli-messaging/build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
     exclude("junit")
   }
   implementation("com.google.code.gson:gson:2.10.1")
-  implementation("com.google.guava:guava:31.1-jre")
+  implementation("com.google.guava:guava:33.2.1-jre")
 
   testImplementation("org.hamcrest:hamcrest:2.2")
   testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
