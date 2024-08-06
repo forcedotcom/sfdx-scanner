ded8f894db0b740ec00e661cba8dc47d7b32fd07
Joshua Feingold <jfeingold35@gmail.com>
2024-08-06 09:56:45 -0500

    CHANGE @W-16344034@ Switching from globby to fast-glob.

--- src/lib/factories/PathStartFactory.ts
+++ src/lib/factories/PathStartFactory.ts
@@ -1,6 +1,5 @@
-import globby from 'globby';
-import normalizePath from 'normalize-path';
 import path from 'node:path';
+import * as fg from 'fast-glob';
 import {getMessage, BundleName} from '../messages';
 
 export class PathStartFactory {
@@ -10,10 +9,11 @@ export class PathStartFactory {
 		}
 		const processedPaths: string[] = [];
 		for (const pathStart of pathStarts) {
-			// We need to normalize the path here, because otherwise ordinary Windows paths will be misidentified as Globs
-			// due to the path separator (\) also being the Glob syntax escape character.
-			const normalizedPathStart: string = normalizePath(pathStart);
-			if (globby.hasMagic(normalizedPathStart)) {
+			// Globs assume the path separator character is '/' and treat '\\' as an escape character. So on OSs where
+			// the path separator character isn't '/', we need to convert paths into Glob syntax before checking whether
+			// they're actually Globs, to prevent misidentification.
+			const normalizedPathStart: string = path.sep === '/' ? pathStart : fg.convertPathToPattern(pathStart);
+			if (fg.isDynamicPattern(normalizedPathStart)) {
 				// Globs and method-level targeting are mutually exclusive.
 				if (path.basename(pathStart).includes('#')) {
 					throw new Error(getMessage(BundleName.PathStartFactory, 'error.glob-method-conflict', [pathStart]));
@@ -21,7 +21,7 @@ export class PathStartFactory {
 				// NOTE: We are returning the strict results of the Glob here, meaning that the paths will be normalized
 				// to UNIX style. If this is discovered to cause problems downstream on Windows, then we'll need to
 				// identify a fix.
-				processedPaths.push(...(await globby(normalizedPathStart)));
+				processedPaths.push(...await fg.glob(normalizedPathStart));
 			} else {
 				processedPaths.push(pathStart);
 			}
--- src/lib/factories/WorkspaceFactory.ts
+++ src/lib/factories/WorkspaceFactory.ts
@@ -1,5 +1,5 @@
-import globby from 'globby';
-import normalizePath from 'normalize-path';
+import path from 'node:path';
+import * as fg from 'fast-glob';
 import {CodeAnalyzer, Workspace} from '@salesforce/code-analyzer-core';
 
 export class WorkspaceFactory {
@@ -7,14 +7,15 @@ export class WorkspaceFactory {
 	public async create(core: CodeAnalyzer, workspacePaths: string[]): Promise<Workspace> {
 		const processedPaths: string[] = [];
 		for (const workspacePath of workspacePaths) {
-			// We need to normalize the path here, because otherwise ordinary Windows paths will be misidentified as Globs
-			// due to the path separator (\) also being the Glob syntax escape character.
-			const normalizedWorkspacePath: string = normalizePath(workspacePath);
-			if (globby.hasMagic(normalizedWorkspacePath)) {
+			// Globs assume the path separator character is '/' and treat '\\' as an escape character. So on OSs where
+			// the path separator character isn't '/', we need to convert paths into Glob syntax before checking whether
+			// they're actually Globs, to prevent misidentification.
+			const normalizedWorkspacePath: string = path.sep === '/' ? workspacePath : fg.convertPathToPattern(workspacePath);
+			if (fg.isDynamicPattern(normalizedWorkspacePath)) {
 				// NOTE: We are returning the strict results of the Glob here, meaning that the paths will be normalized
 				// to UNIX style. If this is discovered to cause problems downstream on Windows, then we'll need to
 				// identify a fix.
-				processedPaths.push(...(await globby(normalizedWorkspacePath)));
+				processedPaths.push(...await fg.glob(normalizedWorkspacePath));
 			} else {
 				processedPaths.push(workspacePath);
 			}
