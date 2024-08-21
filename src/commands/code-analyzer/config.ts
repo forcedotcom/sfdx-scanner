58d61cbc34c93b04de4cfdc0aab11c85fa2dbc10
Joshua Feingold <jfeingold35@gmail.com>
2024-08-21 15:58:20 -0500

    NEW @W-16551023@ Refactored ConfigAction layer.

--- src/commands/code-analyzer/config.ts
+++ src/commands/code-analyzer/config.ts
@@ -1,9 +1,14 @@
+import {CodeAnalyzerConfig, RuleSelection} from "@salesforce/code-analyzer-core";
 import {Flags, SfCommand} from '@salesforce/sf-plugins-core';
 import {ConfigAction, ConfigDependencies} from '../../lib/actions/ConfigAction';
 import {ConfigFileWriter} from '../../lib/writers/ConfigWriter';
 import {ConfigRawYamlViewer} from '../../lib/viewers/ConfigViewer';
+import {CodeAnalyzerConfigFactoryImpl} from '../../lib/factories/CodeAnalyzerConfigFactory';
+import {EnginePluginsFactoryImpl} from '../../lib/factories/EnginePluginsFactory';
 import {BundleName, getMessage, getMessages} from '../../lib/messages';
 import {LogEventDisplayer} from '../../lib/listeners/LogEventListener';
+import {RuleSelectionProgressSpinner} from '../../lib/listeners/ProgressEventListener';
+import {DummyConfigModel} from '../../lib/models/ConfigModel';
 import {Displayable, UxDisplay} from '../../lib/Display';
 
 export default class ConfigCommand extends SfCommand<void> implements Displayable {
@@ -51,6 +56,7 @@ export default class ConfigCommand extends SfCommand<void> implements Displayabl
 	};
 
 	public async run(): Promise<void> {
+		this.warn('This command is still a work-in-progress. Currently it can only generate a fixed config file.');
 		const parsedFlags = (await this.parse(ConfigCommand)).flags;
 		const dependencies: ConfigDependencies = this.createDependencies(parsedFlags['output-file']);
 		const action: ConfigAction = ConfigAction.createAction(dependencies);
@@ -59,9 +65,15 @@ export default class ConfigCommand extends SfCommand<void> implements Displayabl
 
 	protected createDependencies(outputFile?: string): ConfigDependencies {
 		const uxDisplay: UxDisplay = new UxDisplay(this, this.spinner);
+		const modelGeneratorFunction = (rawConfig: CodeAnalyzerConfig, ruleSelection: RuleSelection) => {
+			return DummyConfigModel.fromSelection(rawConfig, ruleSelection);
+		};
 		const dependencies: ConfigDependencies = {
-			display: uxDisplay,
+			configFactory: new CodeAnalyzerConfigFactoryImpl(),
+			pluginsFactory: new EnginePluginsFactoryImpl(),
 			logEventListeners: [new LogEventDisplayer(uxDisplay)],
+			progressEventListeners: [new RuleSelectionProgressSpinner(uxDisplay)],
+			modelGenerator: modelGeneratorFunction,
 			viewer: new ConfigRawYamlViewer(uxDisplay)
 		};
 		if (outputFile) {
