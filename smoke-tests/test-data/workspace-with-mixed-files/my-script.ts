9b7d80c9b7733e292dd310c5a16c66cfabd260dc
Joshua Feingold <jfeingold35@gmail.com>
2024-10-18 14:41:34 -0500

    NEW @W-17014756@ Introduced functional smoke tests

---
+++ smoke-tests/test-data/workspace-with-mixed-files/my-script.ts
@@ -0,0 +1,6 @@
+// TODO This is just something to trip a Regex Rule looking for this kind of statement
+
+console.log('asdfasdfasdsasdf');
+const s: string = 'asdfasdf';
+
+console.log(`s is ${s}`);
